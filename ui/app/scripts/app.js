a42db0ef1040900b69ec5fb77a4bb680683b1d01
Nabil Adouani <nabil.adouani@gmail.com>
2017-01-30 11:09:27 +0100

    CRemove references to CodeMirror library

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -6,7 +6,7 @@ angular.module('theHiveDirectives', []);
 angular.module('thehive', ['ngAnimate', 'ngMessages', 'ui.bootstrap', 'ui.router',
         'theHiveControllers', 'theHiveServices', 'theHiveFilters',
         'theHiveDirectives', 'yaru22.jsonHuman', 'timer', 'angularMoment', 'ngCsv', 'ngTagsInput', 'btford.markdown',
-        'ngResource', 'ui.codemirror', 'ui-notification', 'angularjs-dropdown-multiselect', 'base64', 'angular-clipboard',
+        'ngResource', 'ui-notification', 'angularjs-dropdown-multiselect', 'base64', 'angular-clipboard',
         'LocalStorageModule', 'angular-markdown-editor', 'hc.marked', 'hljs', 'ui.ace', 'angular-page-loader', 'naif.base64', 'images-resizer'
     ])
     .config(function($resourceProvider) {
