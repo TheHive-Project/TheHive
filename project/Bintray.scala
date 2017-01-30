a27ea3cd8ecaf046218c6c96c9fb0dd35b316e64
To-om <toom@the-hive-project.org>
2017-01-30 15:22:51 +0100

    Remove latest version before publish it

--- project/Bintray.scala
+++ project/Bintray.scala
@@ -14,14 +14,14 @@ import bintray.BintrayCredentials
 import bintray.BintrayKeys.{ bintrayEnsureCredentials, bintrayOrganization, bintrayRepository, bintrayPackage }
 import com.typesafe.sbt.packager.universal.UniversalPlugin.autoImport.Universal
 
-object PublishToBinTray {
+object PublishToBinTray extends Plugin {
   val publishRelease = taskKey[Unit]("Publish binary in bintray")
   val publishLatest = taskKey[Unit]("Publish latest binary in bintray")
 
-  lazy val settings = Seq(
+  override def settings = Seq(
     publishRelease := {
       val file = (packageBin in Universal).value
-      publish(file.getName,
+      btPublish(file.getName,
         file,
         bintrayEnsureCredentials.value,
         bintrayOrganization.value,
@@ -35,27 +35,42 @@ object PublishToBinTray {
       val latestName = file.getName.replace(version.value, "latest")
       if (latestName == file.getName)
         sLog.value.warn(s"Latest package name can't be built using package name [$latestName], publish aborted")
-      else
-        publish(latestName,
+      else {
+        removeVersion(bintrayEnsureCredentials.value,
+          bintrayOrganization.value,
+          bintrayRepository.value,
+          bintrayPackage.value,
+          "latest", sLog.value)
+        btPublish(latestName,
           file,
           bintrayEnsureCredentials.value,
           bintrayOrganization.value,
           bintrayRepository.value,
           bintrayPackage.value,
-          version.value,
+          "latest",
           sLog.value)
+      }
     })
 
   private def asStatusAndBody = new FunctionHandler({ r => (r.getStatusCode, r.getResponseBody) })
 
-  def publish(filename: String, file: File, credential: BintrayCredentials, org: Option[String], repoName: String, packageName: String, version: String, log: Logger) = {
+  def removeVersion(credential: BintrayCredentials, org: Option[String], repoName: String, packageName: String, version: String, log: Logger) = {
+    val BintrayCredentials(user, key) = credential
+    val client: Client = Client(user, key, new Http())
+    val repo: Client#Repo = client.repo(org.getOrElse(user), repoName)
+    Await.result(repo.get(packageName).version(version).delete(asStatusAndBody), Duration.Inf) match {
+      case (status, body) => log.info(s"Delete version $packageName $version: $status ($body)")
+    }
+  }
+
+  private def btPublish(filename: String, file: File, credential: BintrayCredentials, org: Option[String], repoName: String, packageName: String, version: String, log: Logger) = {
     val BintrayCredentials(user, key) = credential
     val owner: String = org.getOrElse(user)
     val client: Client = Client(user, key, new Http())
     val repo: Client#Repo = client.repo(org.getOrElse(user), repoName)
 
     log.info(s"Uploading $file ...")
-    Await.result(repo.get(packageName).version(version).upload(file.getName, file)(asStatusAndBody), Duration.Inf) match {
+    Await.result(repo.get(packageName).version(version).upload(filename, file)(asStatusAndBody), Duration.Inf) match {
       case (201, _)  => log.info(s"$file was uploaded to $owner/$packageName@$version")
       case (_, fail) => sys.error(s"failed to upload $file to $owner/$packageName@$version: $fail")
     }
