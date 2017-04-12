801107d8fbdc4547c053ca89c6bb2f444c5d084e
Nabil Adouani <nabil.adouani@gmail.com>
2017-04-12 17:28:24 +0200

    #170 Rename the entity type field

--- ui/app/scripts/services/EntitySrv.js
+++ ui/app/scripts/services/EntitySrv.js
@@ -3,24 +3,24 @@
     angular.module('theHiveServices').factory('EntitySrv', function($state) {
         var buildState = function(entity) {
             var state = {};
-            if (entity.type === 'case') {
+            if (entity._type === 'case') {
                 state.name = 'app.case.details';
                 state.params = {
                     caseId: entity.id
                 };
-            } else if (entity.type === 'case_artifact') {
+            } else if (entity._type === 'case_artifact') {
                 state.name = 'app.case.observables-item';
                 state.params = {
                     caseId: entity.case.id,
                     itemId: entity.id
                 };
-            } else if (entity.type === 'case_artifact_job') {
+            } else if (entity._type === 'case_artifact_job') {
                 state.name = 'app.case.observables-item';
                 state.params = {
                     caseId: entity.case_artifact.case.id,
                     itemId: entity.case_artifact.id
                 };
-            } else if (entity.type === 'case_task') {
+            } else if (entity._type === 'case_task') {
                 state.name = 'app.case.tasks-item';
                 state.params = {
                     caseId: entity.case.id,
