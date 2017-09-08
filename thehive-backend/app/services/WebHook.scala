cfef53ea24a7dcbd9a5e44cf038567125b0c6e7c
To-om <toom@thehive-project.org>
2017-09-08 16:13:27 +0200

    #293 really hide stacktrace if connection to web hook fails

--- thehive-backend/app/services/WebHook.scala
+++ thehive-backend/app/services/WebHook.scala
@@ -17,7 +17,7 @@ case class WebHook(name: String, ws: WSRequest)(implicit ec: ExecutionContext) {
 
   def send(obj: JsObject): Unit = ws.post(obj).onComplete {
     case Success(resp) if resp.status / 100 != 2 ⇒ logger.error(s"WebHook returns status ${resp.status} ${resp.statusText}")
-    case Failure(ce: ConnectException)           ⇒ logger.error(s"Connection to WebHook $name error", ce)
+    case Failure(_: ConnectException)            ⇒ logger.error(s"Connection to WebHook $name error")
     case Failure(error)                          ⇒ logger.error("WebHook call error", error)
     case _                                       ⇒
   }
