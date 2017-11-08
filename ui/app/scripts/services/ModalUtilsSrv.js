359b93ebb9b44d9bf869bc03f27a89d787ca3849
Nabil Adouani <nabil.adouani@gmail.com>
2017-11-08 11:51:00 +0100

    #312 Fix the modal confirm service

--- ui/app/scripts/services/ModalUtilsSrv.js
+++ ui/app/scripts/services/ModalUtilsSrv.js
@@ -5,18 +5,18 @@
         this.confirm = function(title, message, config) {
             var modal = $uibModal.open({
                 templateUrl: 'views/partials/utils/confirm.modal.html',
-                controller: function($uibModalInstance, title, message, config) {
+                controller: ['$uibModalInstance', 'title', 'message', 'config', function($uibModalInstance, title, message, config) {
                     this.title = title;
                     this.message = message;
                     this.config = config;
-                    
+
                     this.cancel = function() {
                         $uibModalInstance.dismiss();
                     }
                     this.confirm = function() {
                         $uibModalInstance.close();
                     }
-                },
+                }],
                 controllerAs: '$modal',
                 resolve: {
                     title: function() {
