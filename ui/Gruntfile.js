9ebcdb13221f7322c5e9332c1c248a542531cf5a
Nabil Adouani <nabil.adouani@gmail.com>
2017-12-04 11:43:48 +0100

    #312 Fix layout of series definition form on chrome/linux

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function(grunt) {
             options: {
                 port: 3001,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost',
+                hostname: '0.0.0.0',
                 livereload: 35729
             },
             proxies: [{
