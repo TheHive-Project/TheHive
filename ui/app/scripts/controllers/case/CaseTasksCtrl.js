21944830b3aceeee4380383b883666e1553e9191
Nabil Adouani <nabil.adouani@gmail.com>
2018-08-24 14:02:58 +0200

    #681 Remember the task view configuration

--- ui/app/scripts/controllers/case/CaseTasksCtrl.js
+++ ui/app/scripts/controllers/case/CaseTasksCtrl.js
@@ -4,14 +4,14 @@
         .controller('CaseTaskDeleteCtrl', CaseTaskDeleteCtrl)
         .controller('CaseTasksCtrl', CaseTasksCtrl);
 
-    function CaseTasksCtrl($scope, $state, $stateParams, $q, $uibModal, CaseTabsSrv, PSearchSrv, CaseTaskSrv, UserInfoSrv, NotificationSrv, CortexSrv) {
+    function CaseTasksCtrl($scope, $state, $stateParams, $q, $uibModal, CaseTabsSrv, PSearchSrv, CaseTaskSrv, UserInfoSrv, NotificationSrv, CortexSrv, AppLayoutSrv) {
 
         CaseTabsSrv.activateTab($state.current.data.tab);
 
         $scope.caseId = $stateParams.caseId;
         $scope.state = {
             isNewTask: false,
-            showGrouped: false
+            showGrouped: !!AppLayoutSrv.layout.groupTasks
         };
         $scope.newTask = {
             status: 'Waiting'
@@ -41,6 +41,12 @@
             },
             pageSize: 1000
         });
+
+        $scope.toggleGroupedView = function() {
+            $scope.state.showGrouped = !$scope.state.showGrouped;
+
+            AppLayoutSrv.groupTasks($scope.state.showGrouped);
+        };
 
         $scope.buildTaskGroups = function(tasks) {
             // Sort tasks by order
