30c418e648ccc2911788a311a8231cd566253cce
To-om <toom@thehive-project.org>
2018-11-02 20:45:29 +0100

    Refactor schema definition

--- thehive/app/org/thp/thehive/models/TheHiveSchema.scala
+++ thehive/app/org/thp/thehive/models/TheHiveSchema.scala
@@ -2,7 +2,8 @@ package org.thp.thehive.models
 
 import javax.inject.{Inject, Singleton}
 import org.thp.scalligraph.models.Model.{Edge, Vertex}
-import org.thp.scalligraph.models.{Database, Schema}
+import org.thp.scalligraph.models.{InitialValue, Model, Schema}
+import org.thp.scalligraph.services.VertexSrv
 import org.thp.thehive.services._
 
 @Singleton
@@ -15,9 +16,10 @@ class TheHiveSchema @Inject()(
     val customFieldSrv: CustomFieldSrv,
     val localUserSrv: LocalUserSrv,
     val organisationSrv: OrganisationSrv
-)(implicit db: Database)
-    extends Schema {
-  val auditModel: Vertex[Audit]                   = db.getVertexModel[Audit]
-  val auditedModel: Edge[Audited, Audit, Product] = Audited.model
-
+) extends Schema {
+  val auditModel: Vertex[Audit]                    = Model.vertex[Audit]
+  val auditedModel: Edge[Audited, Audit, Product]  = Audited.model
+  def vertexSrvList: Seq[VertexSrv[_, _]]          = Seq(caseSrv, userSrv, taskSrv, logSrv, impactStatusSrv, customFieldSrv, organisationSrv)
+  override def modelList: Seq[Model]               = vertexSrvList.map(_.model) :+ auditModel :+ auditedModel
+  override def initialValues: Seq[InitialValue[_]] = vertexSrvList.flatMap[InitialValue[_], Seq[InitialValue[_]]](_.getInitialValues)
 }
