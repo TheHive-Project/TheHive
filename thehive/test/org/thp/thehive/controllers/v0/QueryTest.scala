042900af19f75762bb658cedafba36dfefa0e11c
To-om <toom@thehive-project.org>
2019-03-10 22:18:49 +0100

    Reformat code

--- thehive/test/org/thp/thehive/controllers/v0/QueryTest.scala
+++ thehive/test/org/thp/thehive/controllers/v0/QueryTest.scala
@@ -1,6 +1,5 @@
 package org.thp.thehive.controllers.v0
-import javax.inject.Singleton
-import org.scalactic.Good
+
 import org.specs2.mock.Mockito
 import org.thp.scalligraph.controllers.{FObject, FString, Field}
 import org.thp.scalligraph.models.Database
@@ -10,55 +9,39 @@ import play.api.test.PlaySpecification
 
 class QueryTest extends PlaySpecification with Mockito with QueryCtrl {
 
-  @Singleton
   val queryExecutor = new TheHiveQueryExecutor(
     mock[CaseSrv],
     mock[TaskSrv],
     mock[Database]
   )
-//  val queryCtrl = new QueryCtrl(thehiveQueryExecutor)
 
   "Controller" should {
     "parse stats query" in {
       val input = Json.parse("""
-{
-  "query": {
-    "_and": [{
-      "_in": {
-        "_field": "status",
-        "_values": ["Waiting", "InProgress"]
-      }
-    }, {
-      "owner": "admin"
-    }]
-  },
-  "stats": [{
-    "_agg": "field",
-    "_field": "status",
-    "_select": [{ "_agg": "count"}]
-  }, {
-    "_agg": "count"
-  }]
-}
+          | {
+          |   "query": {
+          |     "_and": [{
+          |       "_in": {
+          |         "_field": "status",
+          |         "_values": ["waiting", "inProgress"]
+          |       }
+          |     }, {
+          |       "owner": "admin"
+          |     }]
+          |   },
+          |   "stats": [{
+          |     "_agg": "field",
+          |     "_field": "status",
+          |     "_select": [{ "_agg": "count"}]
+          |   }, {
+          |     "_agg": "count"
+          |   }]
+          | }
         """.stripMargin)
 
       val queryOrError = statsParser(FObject("_name" → FString("listTask")))(Field(input)).map(x ⇒ x)
-      queryOrError must_=== Good(Nil)
-      /*
-      Bad(Many(
-      Invalid format for :
-        FObject(Map(_in -> FObject(Map(_field -> FString(status), _values -> FSeq(List(FString(Waiting), FString(InProgress))))))),
-        expected query (query,
-       Invalid format for :
-        FObject(Map(owner -> FString(admin))),
-        expected query (query,
-       Invalid format for :
-        FObject(Map(_in -> FObject(Map(_field -> FString(status), _values -> FSeq(List(FString(Waiting), FString(InProgress))))))),
-        expected query (query,
-       Invalid format for :
-        FObject(Map(owner -> FString(admin))),
-        expected query (query))
-     */
+      queryOrError.isGood must beTrue
+      queryOrError.get must not be empty
     }
   }
 }
