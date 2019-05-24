9d8eb631aaee99a2909501d19b35fb519b23386e
To-om <toom@thehive-project.org>
2019-05-24 11:32:25 +0200

    Update elastic4play

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
     val reflections = "org.reflections" % "reflections" % "0.9.11"
     val zip4j = "net.lingala.zip4j" % "zip4j" % "1.3.2"
-    val elastic4play = "org.thehive-project" %% "elastic4play" % "1.11.1"
+    val elastic4play = "org.thehive-project" %% "elastic4play" % "1.11.2"
     val akkaCluster = "com.typesafe.akka" %% "akka-cluster" % "2.5.19"
     val akkaClusterTools = "com.typesafe.akka" %% "akka-cluster-tools" % "2.5.19"
   }
