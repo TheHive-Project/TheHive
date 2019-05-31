c9010a896bc6d8265132d1513e08bb918a77d735
Alex <ateilhet@gmail.com>
2019-05-31 15:18:46 +0200

    fix: v0 audit/flow get endpoint temporary fix to avoid error spam

--- thehive/app/org/thp/thehive/controllers/v0/AuditCtrl.scala
+++ thehive/app/org/thp/thehive/controllers/v0/AuditCtrl.scala
@@ -1,22 +1,24 @@
 package org.thp.thehive.controllers.v0
 
-import scala.util.Success
-
-import play.api.libs.json.JsArray
-import play.api.mvc.{Action, AnyContent, Results}
-
 import javax.inject.{Inject, Singleton}
 import org.thp.scalligraph.controllers.EntryPoint
 import org.thp.scalligraph.models.Database
 import org.thp.thehive.services.AuditSrv
+import play.api.libs.json.JsArray
+import play.api.mvc.{Action, AnyContent, Results}
+
+import scala.util.Success
 
 @Singleton
 class AuditCtrl @Inject()(entryPoint: EntryPoint, db: Database, auditSrv: AuditSrv) extends AuditConversion {
 
-  def flow(): Action[AnyContent] =
+  // TODO deal with rootId and double check the purpose of count
+  def flow(rootId: Option[String], count: Option[Int]): Action[AnyContent] =
     entryPoint("audit flow")
       .authTransaction(db) { _ ⇒ implicit graph ⇒
         val audits = auditSrv.initSteps.list.toList.map(_.toJson)
-        Success(Results.Ok(JsArray(audits)))
+        Success(Results.Ok(JsArray(
+          if (count.isDefined) audits.take(count.get) else audits
+        )))
       }
 }
