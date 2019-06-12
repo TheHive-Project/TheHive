42a856bd69e010b2e18b4079c6005585647454e5
Alex <ateilhet@gmail.com>
2019-06-12 11:26:28 +0200

    wip: force delete a case v0 with cascade delete data share check

--- thehive/app/org/thp/thehive/services/DataSrv.scala
+++ thehive/app/org/thp/thehive/services/DataSrv.scala
@@ -1,9 +1,9 @@
 package org.thp.thehive.services
 
-import gremlin.scala.{Graph, GremlinScala, Vertex}
+import gremlin.scala.{Graph, GremlinScala, P, Vertex}
 import javax.inject.{Inject, Singleton}
-import org.thp.scalligraph.models.{BaseVertexSteps, Database}
-import org.thp.scalligraph.services.VertexSrv
+import org.thp.scalligraph.models._
+import org.thp.scalligraph.services._
 import org.thp.thehive.models._
 
 @Singleton
@@ -12,10 +12,14 @@ class DataSrv @Inject()()(implicit db: Database) extends VertexSrv[Data, DataSte
 }
 
 class DataSteps(raw: GremlinScala[Vertex])(implicit db: Database, graph: Graph) extends BaseVertexSteps[Data, DataSteps](raw) {
-  override def newInstance(raw: GremlinScala[Vertex]): DataSteps = new DataSteps(raw)
-
   def remove(): Unit = {
     raw.drop().iterate()
     ()
   }
+
+  def observables = new ObservableSteps(raw.inTo[ObservableData])
+
+  def notShared: DataSteps = newInstance(raw.filter(_.inTo[ObservableData].limit(2).count().is(P.lte(1))))
+
+  override def newInstance(raw: GremlinScala[Vertex]): DataSteps = new DataSteps(raw)
 }
--- thehive/app/org/thp/thehive/services/LogSrv.scala
+++ thehive/app/org/thp/thehive/services/LogSrv.scala
@@ -78,6 +78,7 @@ class LogSteps(raw: GremlinScala[Vertex])(implicit db: Database, graph: Graph) e
 
   def remove(id: String): Unit = {
     raw.has(Key("_id") of id).drop().iterate()
+    ()
   }
 
   def richLog: ScalarSteps[RichLog] =
