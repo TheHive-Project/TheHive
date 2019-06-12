960e032c0d3e77b392f8931abedb5dc265af067c
To-om <toom@thehive-project.org>
2019-06-12 11:38:51 +0200

    Use previous data if value is the same

--- thehive/app/org/thp/thehive/services/DataSrv.scala
+++ thehive/app/org/thp/thehive/services/DataSrv.scala
@@ -1,17 +1,25 @@
 package org.thp.thehive.services
 
-import gremlin.scala.{Graph, GremlinScala, P, Vertex}
+import gremlin.scala.{Graph, GremlinScala, Key, P, Vertex}
 import javax.inject.{Inject, Singleton}
-import org.thp.scalligraph.models._
-import org.thp.scalligraph.services._
+import org.thp.scalligraph.auth.AuthContext
+import org.thp.scalligraph.models.{BaseVertexSteps, Database, Entity}
+import org.thp.scalligraph.services.{VertexSrv, _}
 import org.thp.thehive.models._
 
 @Singleton
 class DataSrv @Inject()()(implicit db: Database) extends VertexSrv[Data, DataSteps] {
   override def steps(raw: GremlinScala[Vertex])(implicit graph: Graph): DataSteps = new DataSteps(raw)
+
+  override def create(e: Data)(implicit graph: Graph, authContext: AuthContext): Data with Entity =
+    initSteps
+      .getByData(e.data)
+      .headOption()
+      .getOrElse(super.create(e))
 }
 
 class DataSteps(raw: GremlinScala[Vertex])(implicit db: Database, graph: Graph) extends BaseVertexSteps[Data, DataSteps](raw) {
+
   def remove(): Unit = {
     raw.drop().iterate()
     ()
@@ -22,4 +30,5 @@ class DataSteps(raw: GremlinScala[Vertex])(implicit db: Database, graph: Graph) 
   def notShared: DataSteps = newInstance(raw.filter(_.inTo[ObservableData].limit(2).count().is(P.lte(1))))
 
   override def newInstance(raw: GremlinScala[Vertex]): DataSteps = new DataSteps(raw)
+  def getByData(data: String): DataSteps                         = newInstance(raw.has(Key("data") of data))
 }
