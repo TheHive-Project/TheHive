89da31427f4a9536d2f07f03d01f813d06820fdf
To-om <toom@thehive-project.org>
2019-06-19 14:04:16 +0200

    Use DTO on controller

--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/AnalyzerCtrl.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/AnalyzerCtrl.scala
@@ -1,16 +1,18 @@
 package org.thp.thehive.connector.cortex.controllers.v0
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, Results}
+
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import javax.inject.{Inject, Singleton}
+import org.thp.cortex.dto.v0.AnalyzerConversion
 import org.thp.scalligraph.controllers.EntryPoint
 import org.thp.scalligraph.models.Database
 import org.thp.thehive.connector.cortex.services.AnalyzerSrv
 import org.thp.thehive.controllers.v0.QueryCtrl
-import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, Results}
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class AnalyzerCtrl @Inject()(
@@ -20,15 +22,16 @@ class AnalyzerCtrl @Inject()(
     val queryExecutor: CortexQueryExecutor,
     implicit val system: ActorSystem,
     mat: Materializer
-) extends QueryCtrl {
+) extends QueryCtrl
+    with AnalyzerConversion {
 
   def list: Action[AnyContent] =
     entryPoint("list analyzer")
       .asyncAuth { implicit request ⇒
-        {
-          analyzerSrv
-            .listAnalyzer
-            .map(l ⇒ Results.Ok(Json.toJson(l)))
-        }
+        analyzerSrv
+          .listAnalyzer
+          .map { analyzers ⇒
+            Results.Ok(Json.toJson(analyzers.map(toOutputAnalyzer)))
+          }
       }
 }
--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/services/AnalyzerSrv.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/services/AnalyzerSrv.scala
@@ -1,34 +1,38 @@
 package org.thp.thehive.connector.cortex.services
 
-import akka.actor.ActorSystem
-import akka.stream.Materializer
-import javax.inject.{Inject, Singleton}
-import org.thp.cortex.client.CortexConfig
-import org.thp.cortex.dto.v0.{AnalyzerConversion, OutputAnalyzer}
-
 import scala.concurrent.{ExecutionContext, Future}
 
+import play.api.Logger
+
+import javax.inject.{Inject, Singleton}
+import org.thp.cortex.client.CortexConfig
+import org.thp.cortex.dto.client.OutputCortexAnalyzer
+
 @Singleton
-class AnalyzerSrv @Inject()(cortexConfig: CortexConfig, implicit val ex: ExecutionContext, system: ActorSystem, mat: Materializer)
-    extends AnalyzerConversion {
+class AnalyzerSrv @Inject()(cortexConfig: CortexConfig, implicit val ex: ExecutionContext) {
+
+  lazy val logger = Logger(getClass)
 
   /**
     * Lists the Cortex analyzers from all CortexClients
     *
     * @return
     */
-  def listAnalyzer: Future[Seq[OutputAnalyzer]] =
+  def listAnalyzer: Future[Seq[OutputCortexAnalyzer]] =
     Future
-      .sequence(cortexConfig.instances.map(_.listAnalyser))
+      .traverse(cortexConfig.instances) { cortexInstance ⇒
+        cortexInstance.listAnalyser.recover {
+          case error ⇒
+            logger.error(s"List Cortex analyzers fails on ${cortexInstance.name}", error)
+            Nil
+        }
+      }
       .map { listOfListOfAnalyzers ⇒
         val analysers = listOfListOfAnalyzers.flatten
         analysers
           .groupBy(_.name)
           .values
-          .map(_.reduceLeft((a1, a2) ⇒
-            a1.copy(cortexIds = Some(a1.cortexIds.getOrElse(Nil) ::: a2.cortexIds.getOrElse(Nil)))
-          ))
-          .map(toOutputAnalyzer)
+          .map(_.reduceLeft((a1, a2) ⇒ a1.copy(cortexIds = Some(a1.cortexIds.getOrElse(Nil) ::: a2.cortexIds.getOrElse(Nil)))))
           .toSeq
       }
 }
