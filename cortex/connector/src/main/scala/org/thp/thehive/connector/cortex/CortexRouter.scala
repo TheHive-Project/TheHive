38a14234eb997e8899f7bfdb1a6d638fa94d675f
To-om <toom@thehive-project.org>
2019-06-19 14:03:36 +0200

    Add debug router

--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/CortexRouter.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/CortexRouter.scala
@@ -2,7 +2,7 @@ package org.thp.thehive.connector.cortex
 
 import play.api.Logger
 import play.api.mvc.DefaultActionBuilder
-import play.api.routing.{Router, SimpleRouter}
+import play.api.routing.Router
 
 import javax.inject.{Inject, Provider, Singleton}
 import org.thp.thehive.connector.cortex.controllers.v0
@@ -10,10 +10,8 @@ import org.thp.thehive.connector.cortex.controllers.v0
 @Singleton
 class CortexRouter @Inject()(routerV0: v0.Router, actionBuilder: DefaultActionBuilder) extends Provider[Router] {
 
-  lazy val logger = Logger(getClass)
+  lazy val logger      = Logger(getClass)
   lazy val get: Router =
-    SimpleRouter(
-//      routerV1.withPrefix("/api/cortex/v1/").routes orElse
-      routerV0.withPrefix("/api/connector/cortex/").routes
-    )
+    //routerV1.withPrefix("/api/cortex/v1/") orElse
+    routerV0.withPrefix("/api/connector/cortex/")
 }
