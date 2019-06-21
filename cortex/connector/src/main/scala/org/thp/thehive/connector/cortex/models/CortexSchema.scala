d5a767153b1dadb2a24c90bffe11cd0d74680417
To-om <toom@thehive-project.org>
2019-06-21 11:23:36 +0200

    Fix classloader problem in schema construction

--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/models/CortexSchema.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/models/CortexSchema.scala
@@ -1,9 +1,41 @@
 package org.thp.thehive.connector.cortex.models
 
 import scala.collection.JavaConverters._
+import scala.reflect.runtime.{universe ⇒ ru}
+
+import play.api.Logger
 
 import javax.inject.{Inject, Singleton}
-import org.thp.scalligraph.models.ReflectionSchema
+import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
+import org.reflections.util.ConfigurationBuilder
+import org.thp.scalligraph.models.{HasModel, Model, Schema}
 
 @Singleton
-class CortexSchema @Inject()() extends ReflectionSchema(getClass.getClassLoader, "org.thp.thehive.connector.cortex.models")
+class CortexSchema @Inject()() extends Schema {
+
+  lazy val logger   = Logger(getClass)
+  val rm: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
+  logger.info(s"Search models in org.thp.thehive.connector.cortex.models")
+
+  lazy val reflectionClasses = new Reflections(
+    new ConfigurationBuilder()
+      .forPackages("org.thp.thehive.connector.cortex.models")
+      .addClassLoaders(getClass.getClassLoader)
+      .setExpandSuperTypes(true)
+      .setScanners(new SubTypesScanner(false))
+  )
+
+  override lazy val modelList: Seq[Model] = {
+    reflectionClasses
+      .getSubTypesOf(classOf[HasModel[_]])
+      .asScala
+      .filterNot(c ⇒ java.lang.reflect.Modifier.isAbstract(c.getModifiers))
+      .map { modelClass ⇒
+        val hasModel = rm.reflectModule(rm.classSymbol(modelClass).companion.companion.asModule).instance.asInstanceOf[HasModel[_]]
+        logger.info(s"Loading model ${hasModel.model.label}")
+        hasModel.model
+      }
+      .toSeq
+  }
+}
--- thehive/app/org/thp/thehive/models/TheHiveSchema.scala
+++ thehive/app/org/thp/thehive/models/TheHiveSchema.scala
@@ -1,24 +1,54 @@
 package org.thp.thehive.models
 
-import scala.collection.JavaConverters._
+import java.lang.reflect.Modifier
 
+import scala.collection.JavaConverters._
+import scala.reflect.runtime.{universe ⇒ ru}
+
+import play.api.Logger
 import play.api.inject.Injector
 
 import gremlin.scala.Graph
 import javax.inject.{Inject, Singleton}
+import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
+import org.reflections.util.ConfigurationBuilder
 import org.thp.scalligraph.auth.AuthContext
-import org.thp.scalligraph.models.{InitialValue, ReflectionSchema}
+import org.thp.scalligraph.models.{HasModel, InitialValue, Model, Schema}
 import org.thp.scalligraph.services.VertexSrv
 import org.thp.thehive.services.{OrganisationSrv, ProfileSrv, RoleSrv, UserSrv}
 
 @Singleton
-class TheHiveSchema @Inject()(injector: Injector) extends ReflectionSchema(getClass.getClassLoader, "org.thp.thehive.models") {
+class TheHiveSchema @Inject()(injector: Injector) extends Schema {
+
+  lazy val logger   = Logger(getClass)
+  val rm: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
+
+  val reflectionClasses = new Reflections(
+    new ConfigurationBuilder()
+      .forPackages("org.thp.thehive.models")
+      .addClassLoader(getClass.getClassLoader)
+      .setExpandSuperTypes(true)
+      .setScanners(new SubTypesScanner(false))
+  )
+
+  override lazy val modelList: Seq[Model] =
+    reflectionClasses
+      .getSubTypesOf(classOf[HasModel[_]])
+      .asScala
+      .filterNot(c ⇒ Modifier.isAbstract(c.getModifiers))
+      .map { modelClass ⇒
+        val hasModel = rm.reflectModule(rm.classSymbol(modelClass).companion.companion.asModule).instance.asInstanceOf[HasModel[_]]
+        logger.info(s"Loading model ${hasModel.model.label}")
+        hasModel.model
+      }
+      .toSeq
 
   override lazy val initialValues: Seq[InitialValue[_]] =
     reflectionClasses
       .getSubTypesOf(classOf[VertexSrv[_, _]])
       .asScala
-      .filterNot(c ⇒ java.lang.reflect.Modifier.isAbstract(c.getModifiers))
+      .filterNot(c ⇒ Modifier.isAbstract(c.getModifiers))
       .toSeq
       .flatMap[InitialValue[_], Seq[InitialValue[_]]] { vertexSrvClass ⇒
         injector.instanceOf(vertexSrvClass).getInitialValues
