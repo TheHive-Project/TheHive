da4ff9b881b46695b08d3be1e4f15228969a424f
Alex <ateilhet@gmail.com>
2019-06-25 17:53:47 +0200

    test: adding get analyzer test

--- cortex/client/src/test/scala/org/thp/cortex/client/CortexClientTest.scala
+++ cortex/client/src/test/scala/org/thp/cortex/client/CortexClientTest.scala
@@ -5,6 +5,7 @@ import org.specs2.mock.Mockito
 import org.thp.cortex.dto.v0.OutputCortexAnalyzer
 import play.api.Logger
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
 import play.api.mvc.{Action, ControllerComponents, Results}
 import play.api.routing.Router
 import play.api.routing.sird._
@@ -12,8 +13,17 @@ import play.api.test.{PlaySpecification, WithServer}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.io.Source
 
 class CortexClientTest extends PlaySpecification with Mockito {
+
+  def getAnalyzers: Seq[OutputCortexAnalyzer] = {
+    val dataSource = Source.fromFile(getClass.getResource("/analyzers.json").getPath)
+    val data       = dataSource.mkString
+    dataSource.close()
+
+    Json.parse(data).as[Seq[OutputCortexAnalyzer]]
+  }
 
   s"CortexClient" should {
     lazy val app = GuiceApplicationBuilder()
@@ -22,6 +32,10 @@ class CortexClientTest extends PlaySpecification with Mockito {
         case play.api.routing.sird.GET(p"/api/analyzer") ⇒
           Action {
             Results.Ok.sendResource("analyzers.json")(GuiceApplicationBuilder().injector.instanceOf[ControllerComponents].fileMimeTypes)
+          }
+        case play.api.routing.sird.GET(p"/api/analyzer/$id") ⇒
+          Action {
+            Results.Ok(Json.toJson(getAnalyzers.find(_.id == id).get))
           }
       })
       .build()
@@ -37,12 +51,17 @@ class CortexClientTest extends PlaySpecification with Mockito {
       override lazy val logger: Logger = mockLogger
     }
 
-    "handle get analyzers properly" in new WithServer(app, port = 3333) {
+    "handle requests properly" in new WithServer(app, port = 3333) {
       val analyzers: Seq[OutputCortexAnalyzer] = await(client.listAnalyser)
 
       analyzers.length shouldEqual 2
       analyzers.head.cortexIds must beSome(List(clientName))
       analyzers.head.name shouldEqual "anaTest1"
+
+      val oneAnalyzer: OutputCortexAnalyzer = await(client.getAnalyzer("anaTest2"))
+
+      oneAnalyzer.id shouldEqual "anaTest2"
+      oneAnalyzer.name shouldEqual "anaTest2"
     }
   }
 
