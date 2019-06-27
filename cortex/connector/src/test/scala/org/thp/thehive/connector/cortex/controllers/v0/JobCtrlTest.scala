1ba5bc8a31199f7ea30d4943bc853764d2fbc3a9
Alex <ateilhet@gmail.com>
2019-06-27 17:26:21 +0200

    test: cortex job ctrl and srv first tests

---
+++ cortex/connector/src/test/scala/org/thp/thehive/connector/cortex/controllers/v0/JobCtrlTest.scala
@@ -0,0 +1,90 @@
+package org.thp.thehive.connector.cortex.controllers.v0
+
+import org.specs2.mock.Mockito
+import org.specs2.specification.core.{Fragment, Fragments}
+import org.thp.scalligraph.AppBuilder
+import org.thp.scalligraph.auth.UserSrv
+import org.thp.scalligraph.controllers.{AuthenticateSrv, TestAuthenticateSrv}
+import org.thp.scalligraph.models.{Database, DatabaseProviders, DummyUserSrv, Schema}
+import org.thp.scalligraph.services.{LocalFileSystemStorageSrv, StorageSrv}
+import org.thp.thehive.connector.cortex.dto.v0.OutputAnalyzer
+import org.thp.thehive.dto.v0.OutputObservable
+import org.thp.thehive.models.{DatabaseBuilder, Permissions, TheHiveSchema}
+import org.thp.thehive.services.LocalUserSrv
+import play.api.libs.json.Json
+import play.api.test.{FakeRequest, PlaySpecification}
+import play.api.{Configuration, Environment}
+
+class JobCtrlTest extends PlaySpecification with Mockito {
+  val dummyUserSrv          = DummyUserSrv(permissions = Permissions.all)
+  val config: Configuration = Configuration.load(Environment.simple())
+
+  Fragments.foreach(new DatabaseProviders(config).list) { dbProvider ⇒
+    val app: AppBuilder = AppBuilder()
+      .bind[UserSrv, LocalUserSrv]
+      .bindToProvider(dbProvider)
+      .bind[AuthenticateSrv, TestAuthenticateSrv]
+      .bind[StorageSrv, LocalFileSystemStorageSrv]
+      .bind[Schema, TheHiveSchema]
+      .addConfiguration("play.modules.disabled = [org.thp.scalligraph.ScalligraphModule, org.thp.thehive.TheHiveModule]")
+
+    step(setupDatabase(app)) ^ specs(dbProvider.name, app) ^ step(teardownDatabase(app))
+  }
+
+  def setupDatabase(app: AppBuilder): Unit =
+    app.instanceOf[DatabaseBuilder].build()(app.instanceOf[Database], dummyUserSrv.initialAuthContext)
+
+  def teardownDatabase(app: AppBuilder): Unit = app.instanceOf[Database].drop()
+
+  def specs(name: String, app: AppBuilder): Fragment = {
+    val jobCtrl: JobCtrl = app.instanceOf[JobCtrl]
+
+    s"[$name] job controller" should {
+      "get a job" in {
+        val requestSearch = FakeRequest("POST", s"/api/connector/cortex/job/_search?range=0-200&sort=-startDate")
+          .withHeaders("user" → "user2", "X-Organisation" → "default")
+          .withJsonBody(Json.parse(s"""
+              {
+                 "query":{}
+              }
+            """.stripMargin))
+        val resultSearch = jobCtrl.search(requestSearch)
+
+        status(resultSearch) shouldEqual 200
+      }
+    }
+  }
+
+//  def getObservables(app: AppBuilder) = {
+//    val requestSearch = FakeRequest("POST", s"/api/case/artifact/_search?range=all&sort=-startDate&nstats=true")
+//      .withHeaders("user" → "user2", "X-Organisation" → "default")
+//      .withJsonBody(Json.parse(s"""
+//              {
+//                "query":{
+//                   "_and":[
+//                      {
+//                         "_and":[
+//                            {
+//                               "_parent":{
+//                                  "_type":"case",
+//                                  "_query":{
+//                                     "_id":"${resultCase._id}"
+//                                  }
+//                               }
+//                            },
+//                            {
+//                               "status":"Ok"
+//                            }
+//                         ]
+//                      }
+//                   ]
+//                }
+//             }
+//            """.stripMargin))
+//    val resultSearch = observableCtrl.search(requestSearch)
+//
+//    status(resultSearch) shouldEqual 200
+//
+//    val resSearchObservables = contentAsJson(resultSearch).as[Seq[OutputObservable]]
+//  }
+}
---
+++ cortex/connector/src/test/scala/org/thp/thehive/connector/cortex/services/JobSrvTest.scala
@@ -0,0 +1,63 @@
+package org.thp.thehive.connector.cortex.services
+
+import org.specs2.mock.Mockito
+import org.specs2.specification.core.{Fragment, Fragments}
+import org.thp.scalligraph.AppBuilder
+import org.thp.scalligraph.auth.UserSrv
+import org.thp.scalligraph.controllers.{AuthenticateSrv, TestAuthenticateSrv}
+import org.thp.scalligraph.models.{Database, DatabaseProviders, DummyUserSrv, Schema}
+import org.thp.scalligraph.services.{LocalFileSystemStorageSrv, StorageSrv}
+import org.thp.thehive.connector.cortex.models.{Job, JobStatus}
+import org.thp.thehive.models.{DatabaseBuilder, Permissions, TheHiveSchema}
+import org.thp.thehive.services.LocalUserSrv
+import play.api.libs.json.{Json, Reads}
+import play.api.test.PlaySpecification
+import play.api.{Configuration, Environment}
+
+import scala.io.Source
+
+class JobSrvTest extends PlaySpecification with Mockito {
+  val dummyUserSrv          = DummyUserSrv(permissions = Permissions.all)
+  val config: Configuration = Configuration.load(Environment.simple())
+
+  Fragments.foreach(new DatabaseProviders(config).list) { dbProvider ⇒
+    val app: AppBuilder = AppBuilder()
+      .bind[UserSrv, LocalUserSrv]
+      .bindToProvider(dbProvider)
+      .bind[AuthenticateSrv, TestAuthenticateSrv]
+      .bind[StorageSrv, LocalFileSystemStorageSrv]
+      .bind[Schema, TheHiveSchema]
+      .addConfiguration("play.modules.disabled = [org.thp.scalligraph.ScalligraphModule, org.thp.thehive.TheHiveModule]")
+
+    step(setupDatabase(app)) ^ specs(dbProvider.name, app) ^ step(teardownDatabase(app))
+  }
+
+  def setupDatabase(app: AppBuilder): Unit =
+    app.instanceOf[DatabaseBuilder].build()(app.instanceOf[Database], dummyUserSrv.initialAuthContext)
+
+  def teardownDatabase(app: AppBuilder): Unit = app.instanceOf[Database].drop()
+
+  def specs(name: String, app: AppBuilder): Fragment = {
+    val jobSrv: JobSrv = app.instanceOf[JobSrv]
+    val db: Database   = app.instanceOf[Database]
+
+    s"[$name] job service" should {
+      "create a job" in db.transaction { implicit graph ⇒
+        val job = jobSrv.create(getJobs.head)(graph, dummyUserSrv.authContext)
+
+        job shouldEqual getJobs.head
+      }
+    }
+  }
+
+  def getJobs: Seq[Job] = {
+    val dataSource = Source.fromFile(getClass.getResource("/jobs.json").getPath)
+    val data       = dataSource.mkString
+    dataSource.close()
+
+    implicit val statusReads: Reads[JobStatus.Value] = Reads.enumNameReads(JobStatus)
+    implicit val reads: Reads[Job]                   = Json.reads[Job]
+
+    Json.parse(data).as[Seq[Job]]
+  }
+}
