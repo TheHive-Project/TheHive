e26141e170a3e6d4f8b0514d01b48269f181f3b5
To-om <toom@thehive-project.org>
2019-07-22 17:20:59 +0200

    JobSrv refactoring

--- cortex/connector/src/test/scala/org/thp/thehive/connector/cortex/services/JobSrvTest.scala
+++ cortex/connector/src/test/scala/org/thp/thehive/connector/cortex/services/JobSrvTest.scala
@@ -1,24 +1,29 @@
 package org.thp.thehive.connector.cortex.services
 
+import java.util.Date
+
+import gremlin.scala.{Key, P}
+import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mock.Mockito
 import org.specs2.specification.core.{Fragment, Fragments}
 import org.thp.cortex.client.{Authentication, CustomWSAPI, FakeCortexClient, KeyAuthentication}
 import org.thp.cortex.dto.v0.CortexOutputJob
 import org.thp.scalligraph.AppBuilder
-import org.thp.scalligraph.auth.UserSrv
+import org.thp.scalligraph.auth.{AuthContext, UserSrv}
 import org.thp.scalligraph.controllers.{AuthenticateSrv, TestAuthenticateSrv}
 import org.thp.scalligraph.models.{Database, DatabaseProviders, DummyUserSrv, Schema}
 import org.thp.scalligraph.services.{LocalFileSystemStorageSrv, StorageSrv}
 import org.thp.thehive.connector.cortex.models.{Job, JobStatus}
 import org.thp.thehive.models.{DatabaseBuilder, Permissions, TheHiveSchema}
-import org.thp.thehive.services.LocalUserSrv
-import play.api.libs.json.{Json, Reads}
+import org.thp.thehive.services.{LocalUserSrv, ObservableSrv}
+import play.api.libs.json.Json
 import play.api.test.PlaySpecification
 import play.api.{Configuration, Environment}
 
+import scala.concurrent.duration.DurationInt
 import scala.io.Source
 
-class JobSrvTest extends PlaySpecification with Mockito with FakeCortexClient {
+class JobSrvTest(implicit executionEnv: ExecutionEnv) extends PlaySpecification with Mockito with FakeCortexClient {
   val dummyUserSrv          = DummyUserSrv(permissions = Permissions.all)
   val config: Configuration = Configuration.load(Environment.simple())
 
@@ -49,63 +54,80 @@ class JobSrvTest extends PlaySpecification with Mockito with FakeCortexClient {
   def shutdownActorSystem(app: AppBuilder): Unit = app.app.actorSystem.terminate()
 
   def specs(name: String, app: AppBuilder): Fragment = {
-    val jobSrv: JobSrv = app.instanceOf[JobSrv]
-    val db: Database   = app.instanceOf[Database]
+    val jobSrv: JobSrv               = app.instanceOf[JobSrv]
+    val observableSrv: ObservableSrv = app.instanceOf[ObservableSrv]
+    val db: Database                 = app.instanceOf[Database]
 
     s"[$name] job service" should {
       implicit lazy val ws: CustomWSAPI      = app.instanceOf[CustomWSAPI]
+      implicit val authContext: AuthContext  = dummyUserSrv.authContext
       implicit lazy val auth: Authentication = KeyAuthentication("test")
 
       "create a job" in db.transaction { implicit graph =>
-        val job = jobSrv.create(getJobs.head)(graph, dummyUserSrv.authContext)
-
-        job shouldEqual getJobs.head
+        val maybeObservable = observableSrv.initSteps.has(Key("message"), P.eq("This domain")).getOrFail()
+        maybeObservable must beSuccessfulTry
+        val observable = maybeObservable.get
+        val j = Job(
+          workerId = "anaTest1",
+          workerName = "anaTest1",
+          workerDefinition = "test",
+          status = JobStatus.Waiting,
+          startDate = new Date(1561625908856L),
+          endDate = new Date(1561625908856L),
+          report = None,
+          cortexId = "test",
+          cortexJobId = "AWuYKFatq3Rtqym9DFmL"
+        )
+        val job = jobSrv.create(j, observable)
+        jobSrv.get(job).observable.getOrFail().map(_._id) must beSuccessfulTry(observable._id)
       }
 
       "handle a finished job" in {
+        val maybeObservable = db.transaction { implicit graph =>
+          observableSrv.initSteps.has(Key("message"), P.eq("hello world")).getOrFail()
+        }
+        maybeObservable must beSuccessfulTry
+        val observable = maybeObservable.get
+
+        val job = Job(
+          workerId = "anaTest2",
+          workerName = "anaTest2",
+          workerDefinition = "test2",
+          status = JobStatus.Waiting,
+          startDate = new Date(1561625908856L),
+          endDate = new Date(1561625908856L),
+          report = None,
+          cortexId = "test",
+          cortexJobId = "LVyYKFstq3Rtrdc9DFmL"
+        )
+        val createdJob = db.transaction { implicit graph =>
+          jobSrv.create(job, observable)
+        }
+
+        val cortexOutputJob: CortexOutputJob = {
+          val dataSource = Source.fromResource("cortex-jobs.json")
+          val data       = dataSource.mkString
+          dataSource.close()
+          Json.parse(data).as[CortexOutputJob]
+        }
+
         withCortexClient { client =>
-          val job             = db.transaction(graph => jobSrv.create(getJobs.last)(graph, dummyUserSrv.authContext))
-          val cortexOutputJob = getCortexJobs.find(_.report.isDefined)
+          jobSrv
+            .finished(createdJob._id, cortexOutputJob, client)
+            .map { updatedJob =>
+              updatedJob.status shouldEqual JobStatus.Success
+              updatedJob.report must beSome
+              updatedJob.report.get \ "artifacts" must beEmpty
+              (updatedJob.report.get \ "full" \ "data").as[String] shouldEqual "imageedit_2_3904987689.jpg"
 
-          cortexOutputJob must beSome
-
-          val updatedJobTry = jobSrv.finished(job._id, cortexOutputJob.get, client)(dummyUserSrv.authContext)
-
-          updatedJobTry must beSuccessfulTry
-
-          val updatedJob = updatedJobTry.get
-
-          updatedJob.status shouldEqual JobStatus.Success
-          updatedJob.report must beSome
-          updatedJob.report.get \ "artifacts" must beEmpty
-          (updatedJob.report.get \ "full" \ "data").as[String] shouldEqual "imageedit_2_3904987689.jpg"
-
-          db.transaction {
-            implicit graph =>
-              jobSrv.get(updatedJob._id).observable.toList must beEmpty
-              jobSrv.get(updatedJob._id).reportObservables.toList.length must equalTo(2)
-          }
+              db.transaction { implicit graph =>
+                jobSrv.get(updatedJob).observable.toList.map(_._id) must contain(exactly(observable._id))
+                jobSrv.get(updatedJob).reportObservables.toList.length must equalTo(2)
+              }
+            }
+            .await(0, 5.seconds)
         }
       }
     }
-  }
-
-  def getJobs: Seq[Job] = {
-    val dataSource = Source.fromFile(getClass.getResource("/jobs.json").getPath)
-    val data       = dataSource.mkString
-    dataSource.close()
-
-    implicit val statusReads: Reads[JobStatus.Value] = Reads.enumNameReads(JobStatus)
-    implicit val reads: Reads[Job]                   = Json.reads[Job]
-
-    Json.parse(data).as[Seq[Job]]
-  }
-
-  def getCortexJobs: Seq[CortexOutputJob] = {
-    val dataSource = Source.fromFile(getClass.getResource("/cortex-jobs.json").getPath)
-    val data       = dataSource.mkString
-    dataSource.close()
-
-    Json.parse(data).as[Seq[CortexOutputJob]]
   }
 }
