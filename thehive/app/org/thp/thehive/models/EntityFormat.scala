86a441e95df6e2947b6936deb757743f0bb2f4c3
Alex <ateilhet@gmail.com>
2019-07-22 11:51:03 +0200

    format: just adding missing implementation for Entity writes TODO

--- thehive/app/org/thp/thehive/models/EntityFormat.scala
+++ thehive/app/org/thp/thehive/models/EntityFormat.scala
@@ -4,9 +4,8 @@ import org.thp.scalligraph.models.Entity
 import play.api.libs.json._
 
 object EntityFormat {
-  implicit val writes: Writes[Entity] = new Writes[Entity] {
-    override def writes(e: Entity): JsValue = e match {
-      case t: Task => Json.toJson(t)
-    }
+  implicit val writes: Writes[Entity] = {
+    case t: Task => Json.toJson(t)
+    case _       => ???
   }
 }
