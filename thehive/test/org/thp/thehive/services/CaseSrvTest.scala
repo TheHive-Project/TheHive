5c76059a81916d6124bb89a0d57551a27884270f
To-om <toom@thehive-project.org>
2019-07-24 19:56:33 +0200

    Fix test

--- thehive/test/org/thp/thehive/services/CaseSrvTest.scala
+++ thehive/test/org/thp/thehive/services/CaseSrvTest.scala
@@ -102,7 +102,7 @@ class CaseSrvTest extends PlaySpecification {
         richCase.severity must_=== 2
         richCase.startDate must_=== new Date(1531667370000L)
         richCase.endDate must beNone
-        richCase.tags must_=== Seq(Tag("t1"), Tag("t2"))
+        richCase.tags must contain(exactly(Tag("t1"), Tag("t2")))
         richCase.flag must_=== false
         richCase.tlp must_=== 2
         richCase.pap must_=== 2
