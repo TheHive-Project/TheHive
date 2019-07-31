6042ae26940b83ca6ca7f370a82a290509d58031
To-om <toom@thehive-project.org>
2019-07-31 18:17:43 +0200

    Fix parser for search query

--- thehive/app/org/thp/thehive/controllers/v0/QueryCtrl.scala
+++ thehive/app/org/thp/thehive/controllers/v0/QueryCtrl.scala
@@ -95,7 +95,7 @@ class QueryCtrl(entryPoint: EntryPoint, db: Database, ctrl: QueryableCtrl, query
   val searchParser: FieldsParser[Query] = FieldsParser[Query]("search") {
     case (_, field) =>
       for {
-        maybeInputFilter <- inputFilterParser.optional(field.get("filter"))
+        maybeInputFilter <- inputFilterParser.optional(field.get("query"))
         filteredQuery = maybeInputFilter
           .map(inputFilter => filterQuery.toQuery(inputFilter))
           .fold(ctrl.initialQuery)(ctrl.initialQuery.andThen)
