8b8cc5d93705a8ee35c56615f5209711efaacb12
To-om <toom@thehive-project.org>
2019-07-31 18:21:04 +0200

    Cortex: fix tests

--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/ActionConversion.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/ActionConversion.scala
@@ -1,21 +1,48 @@
 package org.thp.thehive.connector.cortex.controllers.v0
 
+import java.util.Date
+
 import io.scalaland.chimney.dsl._
 import org.thp.scalligraph.Output
 import org.thp.scalligraph.models.UniMapping
 import org.thp.scalligraph.query.{PublicProperty, PublicPropertyListBuilder}
 import org.thp.scalligraph.services._
-import org.thp.thehive.connector.cortex.dto.v0.OutputAction
-import org.thp.thehive.connector.cortex.models.{ActionContext, RichAction}
+import org.thp.thehive.connector.cortex.dto.v0.{InputAction, OutputAction}
+import org.thp.thehive.connector.cortex.models.{Action, ActionContext, JobStatus, RichAction}
 import org.thp.thehive.connector.cortex.services.ActionSteps
+import play.api.libs.json.JsObject
 
 import scala.language.implicitConversions
 
 object ActionConversion {
 
-  implicit def toOutputAction(a: RichAction): Output[OutputAction] =
+  implicit def fromInputAction(inputAction: InputAction): Action =
+    inputAction
+      .into[Action]
+      .withFieldConst(_.responderName, None)
+      .withFieldConst(_.responderDefinition, None)
+      .withFieldConst(_.status, JobStatus.Unknown)
+      .withFieldComputed(_.parameters, _.parameters.getOrElse(JsObject.empty))
+      .withFieldConst(_.startDate, new Date())
+      .withFieldConst(_.endDate, None)
+      .withFieldConst(_.report, None)
+      .withFieldConst(_.cortexJobId, None)
+      .withFieldConst(_.operations, None)
+      .withFieldComputed(_.objectType, a => toEntityType(a.objectType))
+      .transform
+
+  def toEntityType(t: String): String = t match {
+    case "case"          => "Case"
+    case "case_artifact" => "Observable"
+    case "case_task"     => "Task"
+    case "case_task_log" => "Log"
+    case "alert"         => "Alert"
+  }
+
+  implicit def toOutputAction(action: RichAction): Output[OutputAction] =
     Output[OutputAction](
-      a.into[OutputAction]
+      action
+        .into[OutputAction]
         .withFieldComputed(_.status, _.status.toString)
         .withFieldComputed(_.objectId, _.context._id)
         .withFieldComputed(_.objectType, _.context._model.label)
