e423f33964db45b16d2524a06fc98e55b3bf8b6b
To-om <toom@thehive-project.org>
2019-08-09 18:12:55 +0200

    Send audits ids to stream and notification channels

--- thehive/app/org/thp/thehive/services/AuditSrv.scala
+++ thehive/app/org/thp/thehive/services/AuditSrv.scala
@@ -1,7 +1,7 @@
 package org.thp.thehive.services
 
 import gremlin.scala._
-import javax.inject.{Inject, Singleton}
+import javax.inject.{Inject, Provider, Singleton}
 import org.apache.tinkerpop.gremlin.structure.Transaction.Status
 import org.thp.scalligraph.EntitySteps
 import org.thp.scalligraph.auth.AuthContext
@@ -9,21 +9,26 @@ import org.thp.scalligraph.models.{Entity, _}
 import org.thp.scalligraph.services._
 import org.thp.thehive.models._
 import play.api.libs.json.JsObject
-
 import scala.util.{Success, Try}
+
+import play.api.Logger
+
+import org.thp.thehive.services.notification.{AuditNotificationMessage, NotificationTopic}
 
 case class PendingAudit(audit: Audit, context: Entity, `object`: Option[Entity])
 
 @Singleton
 class AuditSrv @Inject()(
-    userSrv: UserSrv,
+    userSrvProvider: Provider[UserSrv],
     eventSrv: EventSrv
 )(implicit db: Database, schema: TheHiveSchema)
     extends VertexSrv[Audit, AuditSteps] { auditSrv =>
+  lazy val logger                                         = Logger(getClass)
+  lazy val userSrv: UserSrv                               = userSrvProvider.get
   val auditUserSrv                                        = new EdgeSrv[AuditUser, Audit, User]
   val auditedSrv                                          = new EdgeSrv[Audited, Audit, Product]
   val auditContextSrv                                     = new EdgeSrv[AuditContext, Audit, Product]
-  private var pendingAudit: Map[AnyRef, PendingAudit]     = Map.empty
+  private var pendingAudits: Map[AnyRef, PendingAudit]    = Map.empty
   private val pendingAuditsLock                           = new Object
   private var transactionAuditIds: List[(AnyRef, String)] = Nil
   private val transactionAuditIdsLock                     = new Object
@@ -49,18 +54,23 @@ class AuditSrv @Inject()(
   def create(audit: Audit, context: Entity, `object`: Option[Entity])(implicit graph: Graph, authContext: AuthContext): Try[Unit] = {
 
     def createLastPending(tx: AnyRef)(implicit graph: Graph, authContext: AuthContext): Try[Unit] = {
-      val p = pendingAudit(tx)
+      logger.debug("Store last audit")
+      val p = pendingAudits(tx)
       pendingAuditsLock.synchronized {
-        pendingAudit = pendingAudit - tx
+        pendingAudits = pendingAudits - tx
       }
       createFromPending(tx, p.audit.copy(mainAction = true), p.context, p.`object`).map { _ =>
         val (ids, otherTxIds) = transactionAuditIds.partition(_._1 == tx)
         transactionAuditIdsLock.synchronized {
           transactionAuditIds = otherTxIds
         }
-        graph.tx.addTransactionListener {
-          case Status.COMMIT => eventSrv.publish(AuditStreamMessage(ids.map(_._2): _*))
-          case _             =>
+        db.addTransactionListener {
+          case Status.COMMIT =>
+            logger.debug("Sending audit to stream bus and to notification actor")
+            val auditIds = ids.map(_._2)
+            eventSrv.publish(StreamTopic())(AuditStreamMessage(auditIds: _*))
+            eventSrv.publish(NotificationTopic())(AuditNotificationMessage(auditIds: _*))
+          case _ =>
         }
       }
     }
@@ -68,7 +78,8 @@ class AuditSrv @Inject()(
     def createFromPending(tx: AnyRef, audit: Audit, context: Entity, `object`: Option[Entity])(
         implicit graph: Graph,
         authContext: AuthContext
-    ): Try[Unit] =
+    ): Try[Unit] = {
+      logger.debug(s"Store audit entity: $audit")
       for {
         user         <- userSrv.current.getOrFail()
         createdAudit <- create(audit)
@@ -78,12 +89,14 @@ class AuditSrv @Inject()(
       } yield transactionAuditIdsLock.synchronized {
         transactionAuditIds = (tx -> createdAudit._id) :: transactionAuditIds
       }
+    }
 
     def setupCallbacks(tx: AnyRef): Try[Unit] = {
-      graph.tx.addTransactionListener {
+      logger.debug("Setup callbacks for the current transaction")
+      db.addTransactionListener {
         case Status.ROLLBACK =>
           pendingAuditsLock.synchronized {
-            pendingAudit = pendingAudit - tx
+            pendingAudits = pendingAudits - tx
           }
           transactionAuditIdsLock.synchronized {
             transactionAuditIds = transactionAuditIds.filterNot(_._1 == tx)
@@ -95,26 +108,26 @@ class AuditSrv @Inject()(
     }
 
     val tx = db.currentTransactionId(graph)
-    if (unauditedTransactions.contains(tx))
+    if (unauditedTransactions.contains(tx)) {
+      logger.debug(s"Audit is disable to the current transaction, $audit ignored.")
       Success(())
-    else {
-      val p = pendingAudit.get(tx)
+    } else {
+      logger.debug(s"Hold $audit, store previous audit if any")
+      val p = pendingAudits.get(tx)
       pendingAuditsLock.synchronized {
-        pendingAudit = pendingAudit + (tx -> PendingAudit(audit, context, `object`))
+        pendingAudits = pendingAudits + (tx -> PendingAudit(audit, context, `object`))
       }
       p.fold(setupCallbacks(tx))(p => createFromPending(tx, p.audit, p.context, p.`object`))
     }
   }
 
-  def get(auditIds: Seq[String])(implicit graph: Graph): AuditSteps = initSteps.filter(_.hasId(auditIds: _*)) // FIXME:ID
-
   class ObjectAudit[E <: Product, C <: Product] {
 
     def create(entity: E with Entity, context: C with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] =
-      auditSrv.create(Audit("create", entity), context, Some(entity))
+      auditSrv.create(Audit(Audit.create, entity), context, Some(entity))
 
     def update(entity: E with Entity, context: C with Entity, details: JsObject)(implicit graph: Graph, authContext: AuthContext): Try[Unit] =
-      auditSrv.create(Audit("update", entity, Some(details.toString)), context, Some(entity))
+      auditSrv.create(Audit(Audit.update, entity, Some(details.toString)), context, Some(entity))
 
     def delete(entity: E with Entity, context: C with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] = Success(())
     // FIXME there is no context
@@ -124,33 +137,60 @@ class AuditSrv @Inject()(
   class SelfContextObjectAudit[E <: Product] {
 
     def create(entity: E with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] =
-      auditSrv.create(Audit("create", entity), entity, Some(entity))
+      auditSrv.create(Audit(Audit.create, entity), entity, Some(entity))
 
     def update(entity: E with Entity, details: JsObject)(implicit graph: Graph, authContext: AuthContext): Try[Unit] =
-      auditSrv.create(Audit("update", entity, Some(details.toString)), entity, Some(entity))
+      auditSrv.create(Audit(Audit.update, entity, Some(details.toString)), entity, Some(entity))
 
     def delete(entity: E with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] = Success(())
     // FIXME there is no context
 //      auditSrv.create(Audit("update", entity, None), entity, Some(entity))
   }
 
-  def getObject(audit: Audit with Entity)(implicit graph: Graph): Option[Entity] =
-    get(audit).getObject
+  def getObject(audit: Audit with Entity)(implicit graph: Graph): Option[Entity] = get(audit).`object`.headOption()
 
   val `case`            = new SelfContextObjectAudit[Case]
   val task              = new ObjectAudit[Task, Case]
-  val taskInTemplate    = new ObjectAudit[Task, CaseTemplate]
   val observable        = new ObjectAudit[Observable, Case]
-  val observableInAlert = new ObjectAudit[Observable, Alert]
   val log               = new ObjectAudit[Log, Case]
-  val alert             = new SelfContextObjectAudit[Alert]
-  val user              = new SelfContextObjectAudit[User]
   val caseTemplate      = new SelfContextObjectAudit[CaseTemplate]
+  val taskInTemplate    = new ObjectAudit[Task, CaseTemplate]
+  val alert             = new SelfContextObjectAudit[Alert]
+  val observableInAlert = new ObjectAudit[Observable, Alert]
+  val user              = new SelfContextObjectAudit[User]
   val dashboard         = new SelfContextObjectAudit[Dashboard]
 }
 
 @EntitySteps[Audit]
 class AuditSteps(raw: GremlinScala[Vertex])(implicit db: Database, schema: Schema, graph: Graph) extends BaseVertexSteps[Audit, AuditSteps](raw) {
+
+  def organisation: OrganisationSteps =
+    new OrganisationSteps(getOrganisation(raw))
+
+  private def getOrganisation(r: GremlinScala[Vertex]): GremlinScala[Vertex] =
+    r.outTo[AuditContext]
+      .choose[Label, Vertex](
+        on = _.label(),
+        BranchCase("Case", new CaseSteps(_).organisations.raw),
+        BranchCase("CaseTemplate", new TaskSteps(_).`case`.organisations.raw),
+        BranchCase("Alert", new AlertSteps(_).organisation.raw),
+        BranchCase("User", new UserSteps(_).organisations.raw),
+        BranchCase("Dashboard", new DashboardSteps(_).organisation.raw)
+        //          BranchOtherwise(_)
+      )
+
+  def auditContextOrganisation: ScalarSteps[(Audit with Entity, Entity, Organisation with Entity)] =
+    ScalarSteps(
+      raw
+        .project(
+          _.apply(By[Vertex]())
+            .and(By(__[Vertex].outTo[AuditContext]))
+            .and(By(getOrganisation(__[Vertex])))
+        )
+        .map {
+          case (audit, context, organisation) => (audit.as[Audit], context.asEntity, organisation.as[Organisation])
+        }
+    )
 
   def richAudit: ScalarSteps[RichAudit] =
     ScalarSteps(
@@ -198,7 +238,7 @@ class AuditSteps(raw: GremlinScala[Vertex])(implicit db: Database, schema: Schem
 
   def visible(implicit authContext: AuthContext): AuditSteps = newInstance(
     raw.filter(
-      _.outTo[AuditContext]
+      _.outTo[AuditContext]                          // TODO use choose step
         .coalesce(                                   // find organisation
           _.inTo[ShareCase].inTo[OrganisationShare], // case
           _.inTo[ShareTask].inTo[OrganisationShare], // task
@@ -211,11 +251,5 @@ class AuditSteps(raw: GremlinScala[Vertex])(implicit db: Database, schema: Schem
     )
   )
 
-  def getObject: Option[Entity] =
-    raw.outTo[Audited].headOption().flatMap { v =>
-      schema.getModel(v.label()).collect {
-        case model: VertexModel =>
-          model.converter(db, graph).toDomain(v)
-      }
-    }
+  def `object`: ScalarSteps[Entity] = ScalarSteps(raw.outTo[Audited].map(_.asEntity))
 }
