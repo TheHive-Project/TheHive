b559d10e12a154fe2d480c3e599d9fc7cbbb74a4
Léarch <learch@firost.fr>
2019-09-03 10:34:18 +0200

    Support for retreiving user groups directly
    User groups are parsed directly from user infos. The config
    parameter `auth.sso.groups.url` is useless in that case.
    
    Note: an AuthenticationError is raised when no groups are
    available for the user.

--- thehive-backend/app/services/mappers/GroupUserMapper.scala
+++ thehive-backend/app/services/mappers/GroupUserMapper.scala
@@ -3,8 +3,9 @@ package services.mappers
 import javax.inject.Inject
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.parsing.combinator._
 
-import play.api.Configuration
+import play.api.{Configuration, Logger}
 import play.api.libs.json._
 import play.api.libs.ws.WSClient
 
@@ -38,13 +39,73 @@ class GroupUserMapper(
 
   override val name: String = "group"
 
-  override def getUserFields(jsValue: JsValue, authHeader: Option[(String, String)]): Future[Fields] = {
+  private[GroupUserMapper] lazy val logger = Logger(getClass)
 
-    val apiCall = authHeader.fold(ws.url(groupsUrl))(headers ⇒ ws.url(groupsUrl).addHttpHeaders(headers))
-    apiCall.get.flatMap { r ⇒
-      val jsonGroups  = (r.json \ groupAttrName).as[Seq[String]]
-      val mappedRoles = jsonGroups.flatMap(mappings.get).maxBy(_.length)
-      val roles       = if (mappedRoles.nonEmpty) mappedRoles else defaultRoles
+  private class RoleListParser extends RegexParsers {
+    val str     = "[a-zA-Z0-9_]+".r
+    val strSpc  = "[a-zA-Z0-9_ ]+".r
+    val realStr = ("\""~>strSpc<~"\"" | "'"~>strSpc<~"'" | str)
+
+    def expr: Parser[Seq[String]] = {
+      "[" ~ opt(realStr ~ rep(("," | ",") ~ realStr)) ~ "]" ^^ {
+        case _ ~ Some(firstRole ~ list) ~ _ => list.foldLeft(Seq(firstRole)) {
+          case (queue, _ ~ role) => role +: queue
+        }
+        case _ ~ _      => Seq.empty[String]
+      } | opt(realStr) ^^ {
+        case Some(role) => Seq(role)
+        case None       => Seq.empty[String]
+      }
+    }
+
+    def apply(input: String, logger: Logger): Seq[String] = parseAll(expr, input) match {
+      case Success(result, _)  => result
+      case failure : NoSuccess => {
+        logger.error(failure.msg)
+        Seq.empty[String]
+      }
+    }
+  }
+
+  override def getUserFields(jsValue: JsValue, authHeader: Option[(String, String)]): Future[Fields] = {
+    if (groupsUrl == "") {
+      logger.debug(s"No groups endpoint provided (auth.sso.groups.url). Assuming groups were already received.")
+
+      val jsonGroups: Seq[String] = {
+        val groups: JsValue = (jsValue \ groupAttrName).get
+        // Groups received as valid JSON array
+        if (groups.isInstanceOf[JsArray]) {
+          groups.as[Seq[String]]
+        // Group list received as string (invalid JSON, for example: "ROLE" or "['Role 1', ROLE2, 'Role_3']")
+        } else if (groups.isInstanceOf[JsString]) {
+          (new RoleListParser).apply(groups.as[String], logger)
+        // Invalid group list
+        } else {
+          logger.error(s"Invalid group list (${groupAttrName} attribute, value '${groups}', type ${groups.getClass})")
+          Seq.empty[String]
+        }
+      }
+
+      mapGroupsAndBuildUserFields(jsValue, jsonGroups)
+
+    } else {
+      val apiCall = authHeader.fold(ws.url(groupsUrl))(headers ⇒ ws.url(groupsUrl).addHttpHeaders(headers))
+      apiCall.get.flatMap { r ⇒
+        val jsonGroups = (r.json \ groupAttrName).as[Seq[String]]
+        mapGroupsAndBuildUserFields(jsValue, jsonGroups)
+      }
+    }
+  }
+
+  private def mapGroupsAndBuildUserFields(jsValue: JsValue, jsonGroups: Seq[String]): Future[Fields] = {
+    val mappedRoles = jsonGroups.flatMap(mappings.get).flatten.toSet
+    val roles       = if (mappedRoles.nonEmpty) mappedRoles else defaultRoles
+
+    if (roles.isEmpty) {
+      Future.failed(AuthenticationError(s"No matched roles for user."))
+
+    } else {
+      logger.debug(s"Computed roles : ${roles}")
 
       val fields = for {
         login ← (jsValue \ loginAttrName).validate[String]
