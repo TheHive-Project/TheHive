fce66dd049b6e6fd2233afd25544f98f4337c53a
Alex <ateilhet@gmail.com>
2019-09-04 09:55:02 +0200

    feat: quick remove Attachment partial implementation

--- thehive/app/org/thp/thehive/services/AttachmentSrv.scala
+++ thehive/app/org/thp/thehive/services/AttachmentSrv.scala
@@ -52,7 +52,10 @@ class AttachmentSrv @Inject()(configuration: Configuration, storageSrv: StorageS
 
   def stream(attachment: Attachment with Entity): InputStream = storageSrv.loadBinary(attachment._id)
 
-  def cascadeRemove(attachment: Attachment with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] = ??? // TODO
+  def cascadeRemove(attachment: Attachment with Entity)(implicit graph: Graph, authContext: AuthContext): Try[Unit] =
+    // TODO handle Storage data removal
+    Try(get(attachment).remove())
+
 }
 
 @EntitySteps[Attachment]
