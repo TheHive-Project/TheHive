0409b86fdacb1d8453fc94654d6d89edb50cfd97
Nabil Adouani <nabil.adouani@gmail.com>
2019-09-17 14:42:15 +0200

    WIP: Add organisation admin section

---
+++ frontend/app/scripts/controllers/admin/AdminProfilesCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular.module('theHiveControllers').controller('AdminProfilesCtrl',
+        function($scope, $uibModal, appConfig) {
+            var self = this;
+
+        });
+})();
---
+++ frontend/app/scripts/controllers/admin/organisation/OrgListCtrl.js
@@ -0,0 +1,73 @@
+(function() {
+    'use strict';
+
+    angular.module('theHiveControllers').controller('OrgListCtrl',
+        function($scope, $q, $uibModal, OrganisationSrv, NotificationSrv, appConfig) {
+            var self = this;
+
+            self.appConfig = appConfig;
+
+            self.load = function() {
+                OrganisationSrv.list()
+                    .then(function(response) {
+                        self.list = response.data;
+                    })
+                    .catch(function() {
+                        // TODO: Handle error
+                    });
+            };
+
+            self.showNewOrg = function(mode, org) {
+                var modal = $uibModal.open({
+                    controller: 'OrgModalCtrl',
+                    controllerAs: '$modal',
+                    templateUrl: 'views/partials/admin/organisation/list/create.modal.html',
+                    size: 'lg',
+                    resolve: {
+                        organisation: org,
+                        mode: function(){
+                            return mode;
+                        }
+                    }
+                });
+
+                modal.result
+                    .then(function(org) {
+                        if (self.mode === 'edit') {
+                            self.update(org._id, org);
+                        } else {
+                            self.create(org);
+                        }
+                    })
+                    .catch(function(err){
+                        if (!_.isString(err)) {
+                            this.NotificationService.error('Unable to save the organisation.');
+                        }
+                    });
+            };
+
+            self.update = function(orgId, org) {
+                OrganisationSrv.update(orgId, _.pick(org, 'description'))
+                    .then(function(/*response*/) {
+                        self.load();
+                        NotificationSrv.log('Organisation updated successfully', 'success');
+                    })
+                    .catch(function(err) {
+                        NotificationSrv.error('Error', 'Organisation update failed', err.status);
+                    });
+            };
+
+            self.create = function(org) {
+                OrganisationSrv.create(org)
+                    .then(function(/*response*/) {
+                        self.load();
+                        NotificationSrv.log('Organisation created successfully', 'success');
+                    })
+                    .catch(function(err) {
+                        NotificationSrv.error('Error', 'Organisation creation failed', err.status);
+                    });
+            };
+
+            self.load();
+        });
+})();
