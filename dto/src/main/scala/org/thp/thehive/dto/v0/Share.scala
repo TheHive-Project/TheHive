f43a7da607793b4fed338cd45a2434d4cad91ef7
Alex <ateilhet@gmail.com>
2019-10-04 17:36:08 +0200

    wip: PUT share case parsing data input

--- dto/src/main/scala/org/thp/thehive/dto/v0/Share.scala
+++ dto/src/main/scala/org/thp/thehive/dto/v0/Share.scala
@@ -4,10 +4,32 @@ import java.util.Date
 
 import play.api.libs.json.{Format, Json, Writes}
 
-case class InputShare(caseId: String, organisationName: String, profile: String)
+case class InputShare(caseId: String, organisationName: String, profile: String, tasks: String, observables: String)
+
+object TasksFilter extends Enumeration {
+  type TasksFilter = Value
+
+  val all: TasksFilter  = Value("all")
+  val none: TasksFilter = Value("none")
+
+  implicit val format: Format[TasksFilter] = Json.formatEnum(TasksFilter)
+}
+
+object ObservablesFilter extends Enumeration {
+  type ObservablesFilter = Value
+
+  val all: ObservablesFilter  = Value("all")
+  val none: ObservablesFilter = Value("none")
+
+  implicit val format: Format[ObservablesFilter] = Json.formatEnum(ObservablesFilter)
+}
 
 object InputShare {
   implicit val writes: Writes[InputShare] = Json.writes[InputShare]
+
+  val tasksFilterParser = ???
+
+  val observablesFilterParser = ???
 }
 
 case class OutputShare(
