dbe5182ea3c56d36e30d34cfbf49a3dca8e09854
Alex <ateilhet@gmail.com>
2019-10-11 09:23:16 +0200

    wip: patch share disabled atm

--- thehive/app/org/thp/thehive/controllers/v0/Router.scala
+++ thehive/app/org/thp/thehive/controllers/v0/Router.scala
@@ -92,7 +92,7 @@ class Router @Inject()(
     case PUT(p"/case/$id/task/$tId/shares") => shareCtrl.shareTask(tId)
     case PUT(p"/case/task/$tId/shares")     => shareCtrl.shareTask(tId)
     case DELETE(p"case/share/$id")          => shareCtrl.removeShare(id)
-    case PATCH(p"case/share/$id")           => shareCtrl.updateShare(id)
+//    case PATCH(p"case/share/$id")           => shareCtrl.updateShare(id)
 
     case GET(p"/case/task")           => queryExecutor.task.search
     case POST(p"/case/$caseId/task")  => taskCtrl.create(caseId) // Audit ok
