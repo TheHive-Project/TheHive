4d0d3ad3f7e5d41b2c665e7ca1b4e8c115bbe5e3
Alex <ateilhet@gmail.com>
2019-10-28 15:32:58 +0100

    test: AttachmentCtrlTest download zip file

--- thehive/test/org/thp/thehive/controllers/v0/AttachmentCtrlTest.scala
+++ thehive/test/org/thp/thehive/controllers/v0/AttachmentCtrlTest.scala
@@ -7,7 +7,6 @@ import org.thp.scalligraph.AppBuilder
 import org.thp.scalligraph.models.{Database, DatabaseProviders, DummyUserSrv}
 import org.thp.thehive.TestAppBuilder
 import org.thp.thehive.models.{DatabaseBuilder, Permissions}
-import org.thp.thehive.services.AttachmentSrv
 import play.api.test.{FakeRequest, PlaySpecification}
 
 import scala.util.Try
@@ -27,7 +26,6 @@ class AttachmentCtrlTest extends PlaySpecification with Mockito {
 
   def specs(name: String, app: AppBuilder): Fragment = {
     val attachmentCtrl: AttachmentCtrl      = app.instanceOf[AttachmentCtrl]
-    val attachmentSrv: AttachmentSrv        = app.instanceOf[AttachmentSrv]
     implicit val materializer: Materializer = app.instanceOf[Materializer]
 
     "download a simple attachment from his id" in {
@@ -36,7 +34,20 @@ class AttachmentCtrlTest extends PlaySpecification with Mockito {
       val result = attachmentCtrl.download("a4bf1f6be616bf6a0de2ff6264de43a64bb768d38c783ec2bc74b5d4dcf5f889", None)(request)
 
       status(result) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
-      header("Content-Disposition", result) must beSome.which(h => h shouldEqual "attachment; filename=\"a4bf1f6be616bf6a0de2ff6264de43a64bb768d38c783ec2bc74b5d4dcf5f889\"")
+      header("Content-Disposition", result) must beSome.which(
+        h => h shouldEqual "attachment; filename=\"a4bf1f6be616bf6a0de2ff6264de43a64bb768d38c783ec2bc74b5d4dcf5f889\""
+      )
+    }
+
+    "download a zipped attachment from his hash" in {
+      val request = FakeRequest("GET", "/api/v0/datastorezip/a4bf1f6be616bf6a0de2ff6264de43a64bb768d38c783ec2bc74b5d4dcf5f889")
+        .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
+      val result = attachmentCtrl.downloadZip("a4bf1f6be616bf6a0de2ff6264de43a64bb768d38c783ec2bc74b5d4dcf5f889", Some("lol"))(request)
+
+      status(result) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
+      header("Content-Disposition", result) must beSome.which(
+        h => h shouldEqual "attachment; filename=\"lol.zip\""
+      )
     }
   }
 }
