e8fd023d4f0ef42289c699dbd3876a5a116451d0
Alex <ateilhet@gmail.com>
2019-10-29 17:33:21 +0100

    test: AuthenticationCtrl

---
+++ thehive/test/org/thp/thehive/controllers/v0/AuthenticationCtrlTest.scala
@@ -0,0 +1,48 @@
+package org.thp.thehive.controllers.v0
+
+import akka.stream.Materializer
+import org.specs2.mock.Mockito
+import org.specs2.specification.core.{Fragment, Fragments}
+import org.thp.scalligraph.AppBuilder
+import org.thp.scalligraph.models.{Database, DatabaseProviders, DummyUserSrv}
+import org.thp.thehive.TestAppBuilder
+import org.thp.thehive.models.{DatabaseBuilder, Permissions}
+import play.api.libs.json.Json
+import play.api.test.{FakeRequest, NoMaterializer, PlaySpecification}
+
+import scala.util.Try
+
+class AuthenticationCtrlTest extends PlaySpecification with Mockito {
+  val dummyUserSrv               = DummyUserSrv(userId = "admin@thehive.local", permissions = Permissions.all)
+  implicit val mat: Materializer = NoMaterializer
+
+  Fragments.foreach(new DatabaseProviders().list) { dbProvider =>
+    val app: AppBuilder = TestAppBuilder(dbProvider)
+    step(setupDatabase(app)) ^ specs(dbProvider.name, app) ^ step(teardownDatabase(app))
+  }
+
+  def setupDatabase(app: AppBuilder): Try[Unit] =
+    app.instanceOf[DatabaseBuilder].build()(app.instanceOf[Database], dummyUserSrv.getSystemAuthContext)
+
+  def teardownDatabase(app: AppBuilder): Unit = app.instanceOf[Database].drop()
+
+  def specs(name: String, app: AppBuilder): Fragment = {
+    val authenticationCtrl = app.instanceOf[AuthenticationCtrl]
+
+    "login and logout users" in {
+      val request = FakeRequest("POST", "/api/v0/login")
+        .withHeaders("X-Organisation" -> "default")
+        .withJsonBody(
+          Json.parse("""{"user": "user3@thehive.local", "password": "*random*seed*,d57cb64654764ab43edfc76a0a84e4e2fda867e761ab5a82859b00ad8412b2a1"}""")
+        )
+      val result = authenticationCtrl.login()(request)
+
+      status(result) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
+
+      val requestOut = FakeRequest("GET", "/api/v0/logout")
+      val resultOut = authenticationCtrl.logout()(requestOut)
+
+      status(resultOut) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
+    }
+  }
+}
