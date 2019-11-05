f234b20e11461711c159c1cd8c6382e3ffd2cf6f
To-om <toom@thehive-project.org>
2019-11-05 17:15:52 +0100

    Fix analyzer template url

--- cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/Router.scala
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/controllers/v0/Router.scala
@@ -27,21 +27,21 @@ class Router @Inject()(
     case GET(p"/action")                                     => cortexQueryExecutor.action.search
     case GET(p"/action/$entityType<[^/]*>/$entityId<[^/]*>") => actionCtrl.getByEntity(entityType, entityId)
 
-    case GET(p"/analyzer/template/content/$analyzerId<[^/]*>/?.*") => reportCtrl.get(analyzerId)
-    case POST(p"/analyzer/template/_import")                       => reportCtrl.importTemplates // Audit ok
-    case POST(p"/analyzer/template/_search")                       => cortexQueryExecutor.report.search
-    case POST(p"/analyzer/template")                               => reportCtrl.create() // Audit ok
-    case DELETE(p"/analyzer/template/$analyzerTemplateId<[^/]*>")  => reportCtrl.delete(analyzerTemplateId) // Audit ok
-    case GET(p"/analyzer/template/$analyzerTemplateId<[^/]*>")     => reportCtrl.get(analyzerTemplateId)
-    case PATCH(p"/analyzer/template/$analyzerTemplateId<[^/]*>")   => reportCtrl.update(analyzerTemplateId) // Audit ok
+    case GET(p"/analyzer/template/content/$analyzerId<[^/]*>$x*") => reportCtrl.get(analyzerId)
+    case POST(p"/analyzer/template/_import")                      => reportCtrl.importTemplates // Audit ok
+    case POST(p"/analyzer/template/_search")                      => cortexQueryExecutor.report.search
+    case POST(p"/analyzer/template")                              => reportCtrl.create() // Audit ok
+    case DELETE(p"/analyzer/template/$analyzerTemplateId<[^/]*>") => reportCtrl.delete(analyzerTemplateId) // Audit ok
+    case GET(p"/analyzer/template/$analyzerTemplateId<[^/]*>")    => reportCtrl.get(analyzerTemplateId)
+    case PATCH(p"/analyzer/template/$analyzerTemplateId<[^/]*>")  => reportCtrl.update(analyzerTemplateId) // Audit ok
 
-    case GET(p"/report/template/content/$analyzerId<[^/]*>/?.*") => reportCtrl.get(analyzerId)
-    case POST(p"/report/template/_import")                       => reportCtrl.importTemplates // Audit ok
-    case POST(p"/report/template/_search")                       => cortexQueryExecutor.report.search
-    case POST(p"/report/template")                               => reportCtrl.create() // Audit ok
-    case DELETE(p"/report/template/$analyzerTemplateId<[^/]*>")  => reportCtrl.delete(analyzerTemplateId) // Audit ok
-    case GET(p"/report/template/$analyzerTemplateId<[^/]*>")     => reportCtrl.get(analyzerTemplateId)
-    case PATCH(p"/report/template/$analyzerTemplateId<[^/]*>")   => reportCtrl.update(analyzerTemplateId) // Audit ok
+    case GET(p"/report/template/content/$analyzerId<[^/]*>$x*") => reportCtrl.get(analyzerId)
+    case POST(p"/report/template/_import")                      => reportCtrl.importTemplates // Audit ok
+    case POST(p"/report/template/_search")                      => cortexQueryExecutor.report.search
+    case POST(p"/report/template")                              => reportCtrl.create() // Audit ok
+    case DELETE(p"/report/template/$analyzerTemplateId<[^/]*>") => reportCtrl.delete(analyzerTemplateId) // Audit ok
+    case GET(p"/report/template/$analyzerTemplateId<[^/]*>")    => reportCtrl.get(analyzerTemplateId)
+    case PATCH(p"/report/template/$analyzerTemplateId<[^/]*>")  => reportCtrl.update(analyzerTemplateId) // Audit ok
 
     case GET(p"/analyzer/$analyzerId<[^/]*>")    => analyzerCtrl.getById(analyzerId)
     case GET(p"/analyzer")                       => analyzerCtrl.list
