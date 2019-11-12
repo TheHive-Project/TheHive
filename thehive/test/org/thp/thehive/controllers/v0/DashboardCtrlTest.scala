34e4fe25eab08d0a003823890ba5a566ebf22386
Alex <ateilhet@gmail.com>
2019-11-12 10:51:09 +0100

    test: DashboardCtrl update delete and get

--- thehive/test/org/thp/thehive/controllers/v0/DashboardCtrlTest.scala
+++ thehive/test/org/thp/thehive/controllers/v0/DashboardCtrlTest.scala
@@ -31,27 +31,77 @@ class DashboardCtrlTest extends PlaySpecification with Mockito {
     val dashboardCtrl: DashboardCtrl = app.instanceOf[DashboardCtrl]
     val theHiveQueryExecutor         = app.instanceOf[TheHiveQueryExecutor]
 
+    def createDashboard(title: String, description: String, st: String, definition: String) = {
+      val request = FakeRequest("POST", "/api/dashboard")
+        .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
+        .withJsonBody(Json.parse(s"""{"title": "$title", "description": "$description", "status": "$st", "definition": "$definition"}"""))
+      val result = dashboardCtrl.create(request)
+
+      status(result) must equalTo(201).updateMessage(s => s"$s\n${contentAsString(result)}")
+
+      contentAsJson(result).as[OutputDashboard]
+    }
+
     s"$name dashboard controller" should {
-//      def createDashboard(title: String, description: String, status: String, definition: String) = {
-//        val request = FakeRequest("POST", "/api/dashboard")
-//          .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
-//          .withJsonBody(Json.parse(s"""{"title": "$title", "description": "$description"}, "status": "$status"}, "definition": "$definition"}"""))
-//        val result = dashboardCtrl.create(request)
-//
-//        status(result) must equalTo(201).updateMessage(s => s"$s\n${contentAsString(result)}")
-//
-//        contentAsJson(result).as[OutputDashboard]
-//      }
 
       "create a dashboard" in {
-        val request = FakeRequest("POST", "/api/dashboard")
+        val dashboard = createDashboard("title test 1", "desc test 1", "Private", "def test 1")
+
+        dashboard.title shouldEqual "title test 1"
+        dashboard.description shouldEqual "desc test 1"
+        dashboard.status shouldEqual "Private" pendingUntilFixed "Dashboards rights management ongoing"
+        dashboard.definition shouldEqual "def test 1"
+      }
+
+      "get a dashboard if visible" in {
+        val dashboard = createDashboard("title test 2", "desc test 2", "Shared", "def test 2")
+
+        val request = FakeRequest("GET", s"/api/dashboard/${dashboard.id}")
           .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
-          .withJsonBody(Json.parse(s"""{"title": "test 1", "description": "test desc 1", "status": "Private", "definition": "test def 1"}"""))
-        val result = dashboardCtrl.create(request)
+        val result = dashboardCtrl.get(dashboard.id)(request)
 
-        status(result) must equalTo(201).updateMessage(s => s"$s\n${contentAsString(result)}")
+        status(result) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
+        contentAsJson(result).as[OutputDashboard].title shouldEqual "title test 2"
 
-        contentAsJson(result).as[OutputDashboard].title shouldEqual "test 1"
+        val requestFailed = FakeRequest("GET", s"/api/dashboard/${dashboard.id}")
+          .withHeaders("user" -> "user3@thehive.local", "X-Organisation" -> "admin")
+        val resultFailed = dashboardCtrl.get(dashboard.id)(requestFailed)
+
+        status(resultFailed) must equalTo(404).updateMessage(s => s"$s\n${contentAsString(resultFailed)}")
+      }
+
+      "update a dashboard" in {
+        val dashboard = createDashboard("title test 3", "desc test 3", "Shared", "def test 3")
+
+        val request = FakeRequest("PATCH", s"/api/dashboard/${dashboard.id}")
+          .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
+          .withJsonBody(Json.parse("""{"title": "updated", "description": "updated", "status": "Private", "definition": "updated"}"""))
+        val result = dashboardCtrl.update(dashboard.id)(request)
+
+        status(result) must equalTo(200).updateMessage(s => s"$s\n${contentAsString(result)}")
+
+        val updatedDashboard = contentAsJson(result).as[OutputDashboard]
+
+        updatedDashboard.title shouldEqual "updated"
+        updatedDashboard.description shouldEqual "updated"
+        updatedDashboard.status shouldEqual "Private" pendingUntilFixed "Dashboards rights management ongoing"
+        updatedDashboard.definition shouldEqual "updated"
+      }
+
+      "delete a dashboard" in {
+        val dashboard = createDashboard("title test 4", "desc test 4", "Shared", "def test 4")
+
+        val request = FakeRequest("DELETE", s"/api/dashboard/${dashboard.id}")
+          .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
+        val result = dashboardCtrl.delete(dashboard.id)(request)
+
+        status(result) must equalTo(204).updateMessage(s => s"$s\n${contentAsString(result)}")
+
+        val requestGet = FakeRequest("GET", s"/api/dashboard/${dashboard.id}")
+          .withHeaders("user" -> "user1@thehive.local", "X-Organisation" -> "cert")
+        val resultGet = dashboardCtrl.get(dashboard.id)(requestGet)
+
+        status(resultGet) must equalTo(404).updateMessage(s => s"$s\n${contentAsString(resultGet)}")
       }
     }
   }
