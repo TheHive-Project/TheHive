32ebdd3145bc76b1a5ff3de72d060fe04d725c5a
Alex <ateilhet@gmail.com>
2019-11-26 12:00:19 +0100

    test: CaseTemplateSrv add task

--- thehive/test/org/thp/thehive/services/CaseTemplateSrvTest.scala
+++ thehive/test/org/thp/thehive/services/CaseTemplateSrvTest.scala
@@ -29,44 +29,67 @@ class CaseTemplateSrvTest extends PlaySpecification {
     val db: Database                      = app.instanceOf[Database]
     val orgaSrv                           = app.instanceOf[OrganisationSrv]
     val userSrv                           = app.instanceOf[UserSrv]
-    val tagSrv: TagSrv                       = app.instanceOf[TagSrv]
-    val taskSrv: TaskSrv                     = app.instanceOf[TaskSrv]
+    val tagSrv: TagSrv                    = app.instanceOf[TagSrv]
+    val taskSrv: TaskSrv                  = app.instanceOf[TaskSrv]
     implicit val authContext: AuthContext = dummyUserSrv.getSystemAuthContext
+
+    def createTemplate(name: String, desc: String) = {
+      val t = db.tryTransaction(
+        implicit graph =>
+          caseTemplateSrv.create(
+            CaseTemplate(
+              name,
+              name,
+              Some("[CTT]"),
+              Some(desc),
+              Some(2),
+              flag = false,
+              Some(1),
+              Some(3),
+              Some(s"summary $name")
+            ),
+            orgaSrv.getOrFail("cert").get,
+            Seq("""testNamespace.testPredicate="t2"""", """testNamespace.testPredicate="newOne""""),
+            Seq(
+              (
+                Task(s"task case template $name", "group1", None, TaskStatus.Waiting, flag = false, None, None, 0, None),
+                userSrv.get("user5@thehive.local").headOption()
+              )
+            ),
+            Seq(("string1", Some("love")), ("boolean1", Some(false)))
+          )
+      )
+
+      t must beSuccessfulTry
+
+      t.get
+    }
 
     s"[$name] case template service" should {
       "create a case template" in {
-        db.tryTransaction(
-          implicit graph =>
-            caseTemplateSrv.create(
-              CaseTemplate(
-                "case template test 1",
-                "ctt 1",
-                Some("[CTT]"),
-                Some("desc ctt 1"),
-                Some(2),
-                flag = false,
-                Some(1),
-                Some(3),
-                Some("summary ctt1")
-              ),
-              orgaSrv.getOrFail("cert").get,
-              Seq("""testNamespace.testPredicate="t2"""", """testNamespace.testPredicate="newOne""""),
-              Seq(
-                (
-                  Task("task case template 1", "group1", None, TaskStatus.Waiting, flag = false, None, None, 0, None),
-                  userSrv.get("user5@thehive.local").headOption()
-                )
-              ),
-              Seq(("string1", Some("love")), ("boolean1", Some(false)))
-            )
-        ) must beSuccessfulTry
+        val name = "case template test 1"
+        createTemplate(name, "description ctt1")
 
         db.roTransaction(implicit graph => {
           tagSrv.initSteps.getByName("testNamespace", "testPredicate", Some("newOne")).exists() must beTrue
-          taskSrv.initSteps.has("title", "task case template 1").exists() must beTrue
-          val richCT = caseTemplateSrv.initSteps.has("name", "case template test 1").richCaseTemplate.getOrFail().get
+          taskSrv.initSteps.has("title", s"task case template $name").exists() must beTrue
+          val richCT = caseTemplateSrv.initSteps.has("name", name).richCaseTemplate.getOrFail().get
           richCT.customFields.length shouldEqual 2
         })
+      }
+
+      "add a task to a template" in {
+        val caseTemplate = createTemplate("case template test 2", "desc ctt2")
+
+        caseTemplate.tasks.length shouldEqual 1
+
+        val task1 = db.roTransaction(implicit graph => taskSrv.initSteps.has("title", "case 1 task 1").richTask.getOrFail().get)
+
+        db.tryTransaction(implicit graph => caseTemplateSrv.addTask(caseTemplate.caseTemplate, task1.task)) must beSuccessfulTry
+        val updatedCaseTemplate =
+          db.roTransaction(implicit graph => caseTemplateSrv.initSteps.has("name", "case template test 2").richCaseTemplate.getOrFail())
+
+        updatedCaseTemplate.get.tasks must contain(task1)
       }
     }
   }
