153b3156915c6f8e921a85c747d8fe91e37dd49b
To-om <toom@thehive-project.org>
2019-12-17 12:00:42 +0100

    Expose default values in service companion

--- thehive/app/org/thp/thehive/services/ImpactStatusSrv.scala
+++ thehive/app/org/thp/thehive/services/ImpactStatusSrv.scala
@@ -3,18 +3,28 @@ package org.thp.thehive.services
 import gremlin.scala._
 import javax.inject.{Inject, Singleton}
 import org.thp.scalligraph.EntitySteps
-import org.thp.scalligraph.models.Database
+import org.thp.scalligraph.auth.AuthContext
+import org.thp.scalligraph.models.{Database, Entity}
 import org.thp.scalligraph.services.VertexSrv
 import org.thp.scalligraph.steps.StepsOps._
 import org.thp.scalligraph.steps.VertexSteps
 import org.thp.thehive.models.ImpactStatus
 
+import scala.util.Try
+
+object ImpactStatusSrv {
+  val noImpact      = ImpactStatus("NoImpact")
+  val withImpact    = ImpactStatus("WithImpact")
+  val notApplicable = ImpactStatus("NotApplicable")
+
+}
+
 @Singleton
 class ImpactStatusSrv @Inject()(implicit db: Database) extends VertexSrv[ImpactStatus, ImpactStatusSteps] {
   override val initialValues: Seq[ImpactStatus] = Seq(
-    ImpactStatus("NoImpact"),
-    ImpactStatus("WithImpact"),
-    ImpactStatus("NotApplicable")
+    ImpactStatusSrv.noImpact,
+    ImpactStatusSrv.withImpact,
+    ImpactStatusSrv.notApplicable
   )
 
   override def steps(raw: GremlinScala[Vertex])(implicit graph: Graph): ImpactStatusSteps = new ImpactStatusSteps(raw)
@@ -22,6 +32,8 @@ class ImpactStatusSrv @Inject()(implicit db: Database) extends VertexSrv[ImpactS
   override def get(idOrName: String)(implicit graph: Graph): ImpactStatusSteps =
     if (db.isValidId(idOrName)) getByIds(idOrName)
     else initSteps.getByName(idOrName)
+
+  def create(impactStatus: ImpactStatus)(implicit graph: Graph, authContext: AuthContext): Try[ImpactStatus with Entity] = createEntity(impactStatus)
 }
 
 @EntitySteps[ImpactStatus]
--- thehive/app/org/thp/thehive/services/ResolutionStatusSrv.scala
+++ thehive/app/org/thp/thehive/services/ResolutionStatusSrv.scala
@@ -3,26 +3,40 @@ package org.thp.thehive.services
 import gremlin.scala._
 import javax.inject.{Inject, Singleton}
 import org.thp.scalligraph.EntitySteps
-import org.thp.scalligraph.models.Database
+import org.thp.scalligraph.auth.AuthContext
+import org.thp.scalligraph.models.{Database, Entity}
 import org.thp.scalligraph.services.VertexSrv
 import org.thp.scalligraph.steps.StepsOps._
 import org.thp.scalligraph.steps.VertexSteps
 import org.thp.thehive.models.ResolutionStatus
 
+import scala.util.Try
+
+object ResolutionStatusSrv {
+  val indeterminate = ResolutionStatus("Indeterminate")
+  val falsePositive = ResolutionStatus("FalsePositive")
+  val truePositive  = ResolutionStatus("TruePositive")
+  val other         = ResolutionStatus("Other")
+  val duplicated    = ResolutionStatus("Duplicated")
+}
+
 @Singleton
 class ResolutionStatusSrv @Inject()(implicit db: Database) extends VertexSrv[ResolutionStatus, ResolutionStatusSteps] {
   override val initialValues = Seq(
-    ResolutionStatus("Indeterminate"),
-    ResolutionStatus("FalsePositive"),
-    ResolutionStatus("TruePositive"),
-    ResolutionStatus("Other"),
-    ResolutionStatus("Duplicated")
+    ResolutionStatusSrv.indeterminate,
+    ResolutionStatusSrv.falsePositive,
+    ResolutionStatusSrv.truePositive,
+    ResolutionStatusSrv.other,
+    ResolutionStatusSrv.duplicated
   )
   override def steps(raw: GremlinScala[Vertex])(implicit graph: Graph): ResolutionStatusSteps = new ResolutionStatusSteps(raw)
 
   override def get(idOrName: String)(implicit graph: Graph): ResolutionStatusSteps =
     if (db.isValidId(idOrName)) getByIds(idOrName)
     else initSteps.getByName(idOrName)
+
+  def create(resolutionStatus: ResolutionStatus)(implicit graph: Graph, authContext: AuthContext): Try[ResolutionStatus with Entity] =
+    createEntity(resolutionStatus)
 }
 
 @EntitySteps[ResolutionStatus]
