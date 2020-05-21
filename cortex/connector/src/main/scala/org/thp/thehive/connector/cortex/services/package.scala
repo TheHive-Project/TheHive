ec758cfff616fec39fd2cbb5bb64f96259513ef2
To-om <toom@thehive-project.org>
2020-05-21 12:36:26 +0200

    #1348 Include parent objects in responder request

---
+++ cortex/connector/src/main/scala/org/thp/thehive/connector/cortex/services/package.scala
@@ -0,0 +1,11 @@
+package org.thp.thehive.connector.cortex
+
+import org.thp.scalligraph.steps.StepsOps._
+import org.thp.thehive.connector.cortex.models.ObservableJob
+import org.thp.thehive.services.ObservableSteps
+
+package object services {
+  implicit class RichObservableSteps(observableSteps: ObservableSteps) {
+    def jobs: JobSteps = new JobSteps(observableSteps.outTo[ObservableJob].raw)(observableSteps.db, observableSteps.graph)
+  }
+}
