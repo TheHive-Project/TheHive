0b102e3851f2fff0a879985c9ad51214700cb855
Nabil Adouani <nabil.adouani@gmail.com>
2020-05-22 20:22:44 +0200

    #1336 Create a reusable component for custome field editing and refactor the case and alert custom fields sections

---
+++ frontend/app/scripts/components/common/custom-field-input.component.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular.module('theHiveComponents')
+        .component('customFieldInput', {
+            controller: function() {
+                this.updateField = function(newValue) {
+                    this.onUpdate({
+                        fieldName: ['customFields', this.field.reference, this.field.type].join('.'),
+                        value: newValue
+                    });
+                };
+            },
+            controllerAs: '$ctrl',
+            templateUrl: 'views/components/common/custom-field-input.component.html',
+            bindings: {
+                field: '<',
+                value: '=',
+                onUpdate: '&',
+                editable: '<'
+            }
+        });
+})();
