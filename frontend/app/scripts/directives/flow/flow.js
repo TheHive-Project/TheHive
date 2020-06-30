02b29fe076dd829e70ccc57dac03d3fed16a5e6d
Nabil Adouani <nabil.adouani@gmail.com>
2020-06-30 17:55:23 +0200

    #1410 start flow refactor

--- frontend/app/scripts/directives/flow/flow.js
+++ frontend/app/scripts/directives/flow/flow.js
@@ -2,28 +2,34 @@
     'use strict';
 
     angular.module('theHiveDirectives')
-        .directive('flow', function(AuditSrv, AnalyzerInfoSrv, UserSrv, $window) {
+        .directive('flow', function(AuditSrv, AnalyzerInfoSrv, UserSrv) {
             return {
                 restrict: 'E',
                 templateUrl: 'views/directives/flow/flow.html',
+                controller: function($scope, $window) {
+                    this.$onInit = function() {
+                        var rootId = '';
+                        if (angular.isString($scope.root)) {
+                            rootId = $scope.root;
+                        } else {
+                            rootId = 'any';
+                        }
+
+                        this.values = AuditSrv(rootId, parseInt($scope.max), $scope);
+                    }
+
+                    if ($window.opener) {
+                        $scope.targetWindow = $window.opener;
+                    }
+                },
+                controllerAs: '$flow',
                 scope: {
                     'root': '@?',
                     'max': '@?'
                 },
                 link: function(scope) {
-                    var rootId = '';
-                    if (angular.isString(scope.root)) {
-                        rootId = scope.root;
-                    } else {
-                        rootId = 'any';
-                    }
                     scope.getAnalyzerInfo = AnalyzerInfoSrv;
                     scope.getUserInfo = UserSrv.getCache;
-                    scope.values = AuditSrv(rootId, parseInt(scope.max), scope);
-
-                    if ($window.opener) {
-                        scope.targetWindow = $window.opener;
-                    }
                 },
             };
         });
