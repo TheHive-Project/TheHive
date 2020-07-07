909fb25e2e9ba1400874a90ec586171fd66ddd6e
To-om <toom@thehive-project.org>
2020-07-07 13:14:25 +0200

    #1410 Add job object in flow

--- thehive/app/org/thp/thehive/controllers/v0/AuditRenderer.scala
+++ thehive/app/org/thp/thehive/controllers/v0/AuditRenderer.scala
@@ -4,11 +4,12 @@ import java.lang.{Long => JLong}
 import java.util.{Map => JMap}
 
 import gremlin.scala.{__, By, Graph, Key, Vertex}
+import org.thp.scalligraph.models.UniMapping
 import org.thp.scalligraph.steps.StepsOps._
 import org.thp.scalligraph.steps._
 import org.thp.thehive.controllers.v0.Conversion._
 import org.thp.thehive.services._
-import play.api.libs.json.{JsNumber, JsObject}
+import play.api.libs.json.{JsNumber, JsObject, JsString}
 
 import scala.collection.JavaConverters._
 
@@ -50,6 +51,30 @@ trait AuditRenderer {
         case (obs, caseOrAlert) => obs.as[JsObject] + ((caseOrAlert \ "_type").asOpt[String].getOrElse("???") -> caseOrAlert)
       }
 
+  def jobToJson: VertexSteps[_ <: Product] => Traversal[JsObject, JsObject] = { s =>
+    val db = s.db
+    Traversal {
+      s.raw.map { vertex =>
+        JsObject(
+          db.getOptionProperty(vertex, "workerId", UniMapping.string.optional).map(v => "analyzerId"                   -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "workerName", UniMapping.string.optional).map(v => "analyzerName"             -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "workerDefinition", UniMapping.string.optional).map(v => "analyzerDefinition" -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "status", UniMapping.string.optional).map(v => "status"                       -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "startDate", UniMapping.date.optional).map(v => "startDate"                   -> JsNumber(v.getTime)).toList :::
+            db.getOptionProperty(vertex, "endDate", UniMapping.date.optional).map(v => "endDate"                       -> JsNumber(v.getTime)).toList :::
+            db.getOptionProperty(vertex, "cortexId", UniMapping.string.optional).map(v => "cortexId"                   -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "cortexJobId", UniMapping.string.optional).map(v => "cortexJobId"             -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "_createdBy", UniMapping.string.optional).map(v => "_createdBy"               -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "_createdAt", UniMapping.date.optional).map(v => "_createdAt"                 -> JsNumber(v.getTime)).toList :::
+            db.getOptionProperty(vertex, "_updatedBy", UniMapping.string.optional).map(v => "_updatedBy"               -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "_updatedAt", UniMapping.date.optional).map(v => "_updatedAt"                 -> JsNumber(v.getTime)).toList :::
+            db.getOptionProperty(vertex, "_type", UniMapping.string.optional).map(v => "_type"                         -> JsString(v)).toList :::
+            db.getOptionProperty(vertex, "_id", UniMapping.string.optional).map(v => "_id"                             -> JsString(v)).toList
+        )
+      }
+    }
+  }
+
   def auditRenderer: AuditSteps => Traversal[JsObject, JsObject] =
     (_: AuditSteps)
       .coalesce[JsObject](
@@ -61,6 +86,7 @@ trait AuditRenderer {
             BranchCase("Log", logToJson),
             BranchCase("Observable", observableToJson),
             BranchCase("Alert", alertToJson),
+            BranchCase("Job", jobToJson),
             BranchOtherwise(_.constant(JsObject.empty))
           ),
         _.constant(JsObject.empty)
