5ea53a9f2c2b639d5af1872b5f14e5a533426e85
Nabil Adouani <nabil.adouani@gmail.com>
2020-11-08 12:31:25 +0100

    #1625 Redesign the observable flag info in the list view

---
+++ frontend/app/scripts/components/common/observable-flags.component.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    angular.module('theHiveComponents')
+        .component('observableFlags', {
+            controller: function() {
+                this.filterBy = function(fieldName, newValue) {
+                    this.onFilter({
+                        fieldName: fieldName,
+                        value: newValue
+                    });
+                };
+            },
+            controllerAs: '$ctrl',
+            templateUrl: 'views/components/common/observable-flags.component.html',
+            bindings: {
+                observable: '<',
+                onFilter: '&'
+            }
+        });
+})();
