4943e0665e1106e6d5e10ac7b319fe0f71654751
Nabil Adouani <nabil.adouani@gmail.com>
2020-11-09 09:41:11 +0100

    #1625 Use observable-flags component in case links table

--- frontend/app/scripts/components/common/observable-flags.component.js
+++ frontend/app/scripts/components/common/observable-flags.component.js
@@ -16,6 +16,8 @@
             bindings: {
                 observable: '<',
                 inline: '<',
+                hideSimilarity: '<',
+                hideSeen: '<',
                 onFilter: '&'
             }
         });
