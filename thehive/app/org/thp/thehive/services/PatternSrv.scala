172a5d3206c19184e2572ba4b76d346cba1d0f07
To-om <toom@thehive-project.org>
2021-01-28 08:49:34 +0100

    #1752 Fix case visibility

--- thehive/app/org/thp/thehive/services/PatternSrv.scala
+++ thehive/app/org/thp/thehive/services/PatternSrv.scala
@@ -33,9 +33,9 @@ class PatternSrv @Inject() (
   override def getByName(name: String)(implicit graph: Graph): Traversal.V[Pattern] =
     Try(startTraversal.getByPatternId(name)).getOrElse(startTraversal.limit(0))
 
-  def getCasePatterns(caseId: String)(implicit graph: Graph): Try[Seq[String]] =
+  def getCasePatterns(caseId: String)(implicit authContext: AuthContext, graph: Graph): Try[Seq[String]] =
     for {
-      caze <- caseSrv.get(EntityIdOrName(caseId)).getOrFail("Case")
+      caze <- caseSrv.get(EntityIdOrName(caseId)).visible(organisationSrv).getOrFail("Case")
       patterns = caseSrv.get(caze).procedure.pattern.richPattern.toSeq
     } yield patterns.map(_.patternId)
 
