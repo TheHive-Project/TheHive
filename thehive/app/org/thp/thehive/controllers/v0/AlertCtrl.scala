e1fd7ae3e2d6c48c12c859633c8189745a133b8e
To-om <toom@thehive-project.org>
2021-02-05 09:20:05 +0100

    #1779 Fix custom fields property definition

--- thehive/app/org/thp/thehive/controllers/v0/AlertCtrl.scala
+++ thehive/app/org/thp/thehive/controllers/v0/AlertCtrl.scala
@@ -21,10 +21,10 @@ import org.thp.thehive.services.OrganisationOps._
 import org.thp.thehive.services.TagOps._
 import org.thp.thehive.services.UserOps._
 import org.thp.thehive.services._
-import play.api.libs.json.{JsArray, JsObject, Json}
+import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, Results}
 
-import java.util.{Base64, List => JList, Map => JMap}
+import java.util.{Base64, Date, List => JList, Map => JMap}
 import javax.inject.{Inject, Named, Singleton}
 import scala.util.{Failure, Success, Try}
 
@@ -433,21 +433,52 @@ class PublicAlert @Inject() (
         case (FPathElem(_, FPathElem(name, _)), alertSteps) =>
           alertSteps.customFields(EntityIdOrName(name)).jsonValue
         case (_, alertSteps) => alertSteps.customFields.nameJsonValue.fold.domainMap(JsObject(_))
-      }.custom {
-        case (FPathElem(_, FPathElem(name, _)), value, vertex, _, graph, authContext) =>
-          for {
-            c <- alertSrv.getByIds(EntityId(vertex.id))(graph).getOrFail("Alert")
-            _ <- alertSrv.setOrCreateCustomField(c, InputCustomFieldValue(name, Some(value), None))(graph, authContext)
-          } yield Json.obj(s"customField.$name" -> value)
-        case (FPathElem(_, FPathEmpty), values: JsObject, vertex, _, graph, authContext) =>
-          for {
-            c   <- alertSrv.get(vertex)(graph).getOrFail("Alert")
-            cfv <- values.fields.toTry { case (n, v) => customFieldSrv.getOrFail(EntityIdOrName(n))(graph).map(_ -> v) }
-            _   <- alertSrv.updateCustomField(c, cfv)(graph, authContext)
-          } yield Json.obj("customFields" -> values)
+      }
+        .filter {
+          case (FPathElem(_, FPathElem(idOrName, _)), alerts) =>
+            db
+              .roTransaction(implicit graph => customFieldSrv.get(EntityIdOrName(idOrName)).value(_.`type`).getOrFail("CustomField"))
+              .map {
+                case CustomFieldType.boolean => alerts.customFields(EntityIdOrName(idOrName)).value(_.booleanValue)
+                case CustomFieldType.date    => alerts.customFields(EntityIdOrName(idOrName)).value(_.dateValue)
+                case CustomFieldType.float   => alerts.customFields(EntityIdOrName(idOrName)).value(_.floatValue)
+                case CustomFieldType.integer => alerts.customFields(EntityIdOrName(idOrName)).value(_.integerValue)
+                case CustomFieldType.string  => alerts.customFields(EntityIdOrName(idOrName)).value(_.stringValue)
+              }
+              .getOrElse(alerts.constant2(null))
+          case (_, alerts) => alerts.constant2(null)
+        }
+        .converter {
+          case FPathElem(_, FPathElem(idOrName, _)) =>
+            db
+              .roTransaction { implicit graph =>
+                customFieldSrv.get(EntityIdOrName(idOrName)).value(_.`type`).getOrFail("CustomField")
+              }
+              .map {
+                case CustomFieldType.boolean => new Converter[Any, JsValue] { def apply(x: JsValue): Any = x.as[Boolean] }
+                case CustomFieldType.date    => new Converter[Any, JsValue] { def apply(x: JsValue): Any = x.as[Date] }
+                case CustomFieldType.float   => new Converter[Any, JsValue] { def apply(x: JsValue): Any = x.as[Double] }
+                case CustomFieldType.integer => new Converter[Any, JsValue] { def apply(x: JsValue): Any = x.as[Long] }
+                case CustomFieldType.string  => new Converter[Any, JsValue] { def apply(x: JsValue): Any = x.as[String] }
+              }
+              .getOrElse((x: JsValue) => x)
+          case _ => (x: JsValue) => x
+        }
+        .custom {
+          case (FPathElem(_, FPathElem(name, _)), value, vertex, _, graph, authContext) =>
+            for {
+              c <- alertSrv.getByIds(EntityId(vertex.id))(graph).getOrFail("Alert")
+              _ <- alertSrv.setOrCreateCustomField(c, InputCustomFieldValue(name, Some(value), None))(graph, authContext)
+            } yield Json.obj(s"customField.$name" -> value)
+          case (FPathElem(_, FPathEmpty), values: JsObject, vertex, _, graph, authContext) =>
+            for {
+              c   <- alertSrv.get(vertex)(graph).getOrFail("Alert")
+              cfv <- values.fields.toTry { case (n, v) => customFieldSrv.getOrFail(EntityIdOrName(n))(graph).map(_ -> v) }
+              _   <- alertSrv.updateCustomField(c, cfv)(graph, authContext)
+            } yield Json.obj("customFields" -> values)
 
-        case _ => Failure(BadRequestError("Invalid custom fields format"))
-      })
+          case _ => Failure(BadRequestError("Invalid custom fields format"))
+        })
       .property("case", db.idMapping)(_.select(_.`case`._id).readonly)
       .property("imported", UMapping.boolean)(_.select(_.imported).readonly)
       .property("importDate", UMapping.date.optional)(_.select(_.importDate).readonly)
