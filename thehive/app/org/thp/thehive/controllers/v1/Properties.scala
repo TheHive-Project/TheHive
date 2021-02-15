189868ca70489840181e5217782001bb655f2bcd
Robin Riclet <robinriclet@gmail.com>
2021-02-15 16:10:12 +0100

    #1766 Added patternId property to procedure

--- thehive/app/org/thp/thehive/controllers/v1/Properties.scala
+++ thehive/app/org/thp/thehive/controllers/v1/Properties.scala
@@ -18,6 +18,7 @@ import org.thp.thehive.services.LogOps._
 import org.thp.thehive.services.ObservableOps._
 import org.thp.thehive.services.OrganisationOps._
 import org.thp.thehive.services.PatternOps._
+import org.thp.thehive.services.ProcedureOps._
 import org.thp.thehive.services.ShareOps._
 import org.thp.thehive.services.TagOps._
 import org.thp.thehive.services.TaskOps._
@@ -367,6 +368,7 @@ class Properties @Inject() (
       .property("description", UMapping.string)(_.field.updatable)
       .property("occurDate", UMapping.date)(_.field.updatable)
       .property("tactic", UMapping.string)(_.field.updatable)
+      .property("patternId", UMapping.string)(_.select(_.pattern.value(_.patternId)).readonly)
       .build
 
   lazy val profile: PublicProperties =
