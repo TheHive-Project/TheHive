52e6e9e5159225e58006a4669c232fec92ac2f3f
Nabil Adouani <nabil.adouani@gmail.com>
2021-02-16 06:22:09 +0100

    #1766 WIP: Add TTP delete operation

--- frontend/app/scripts/controllers/case/CaseProceduresCtrl.js
+++ frontend/app/scripts/controllers/case/CaseProceduresCtrl.js
@@ -3,7 +3,7 @@
     angular.module('theHiveControllers')
         .controller('CaseProceduresCtrl', CaseProceduresCtrl);
 
-    function CaseProceduresCtrl($scope, $state, $stateParams, $uibModal, ModalUtilsSrv, AttackPatternSrv, FilteringSrv, CaseTabsSrv, ProcedureSrv, PaginatedQuerySrv, NotificationSrv, AppLayoutSrv) {
+    function CaseProceduresCtrl($scope, $state, $stateParams, $uibModal, ModalUtilsSrv, AttackPatternSrv, FilteringSrv, CaseTabsSrv, ProcedureSrv, PaginatedQuerySrv, NotificationSrv) {
         var self = this;
 
         CaseTabsSrv.activateTab($state.current.data.tab);
@@ -36,8 +36,6 @@
         this.addProcedure = function() {
             var modalInstance = $uibModal.open({
                 animation: true,
-                // keyboard: false,
-                // backdrop: 'static',
                 templateUrl: 'views/partials/case/procedures/add-procedure.modal.html',
                 controller: 'AddProcedureModalCtrl',
                 controllerAs: '$modal',
@@ -52,6 +50,11 @@
             return modalInstance.result
                 .then(function() {
                     self.load();
+                })
+                .catch(function(err) {
+                    if(err && !_.isString(err)) {
+                        NotificationSrv.error('ProcedureCtrl', err.data, err.status);
+                    }
                 });
         };
 
@@ -65,28 +68,10 @@
             });
         };
 
-        // $scope.state = {
-        //     isNewTask: false,
-        //     showGrouped: !!AppLayoutSrv.layout.groupTasks
-        // };
-        // $scope.newTask = {
-        //     status: 'Waiting'
-        // };
-        // $scope.taskResponders = null;
-        // $scope.collapseOptions = {};
-
-        // $scope.getAssignableUsers = function(taskId) {
-        //     return [
-        //         {_name: 'getTask', idOrName: taskId},
-        //         {_name: 'assignableUsers'}
-        //     ];
-        // };
-
         this.load = function() {
             self.list = new PaginatedQuerySrv({
                 name: 'case-procedures',
                 root: self.caseId,
-                // objectType: 'case_task',
                 version: 'v1',
                 scope: $scope,
                 sort: self.filtering.context.sort,
@@ -98,6 +83,22 @@
                     {'_name': 'procedures'}
                 ],
                 extraData: ['pattern']
+            });
+        };
+
+        self.remove = function(procedure) {
+            ModalUtilsSrv.confirm('Delete TTP', 'Are you sure you want to delete the selected tactic, technique and procedure?', {
+                okText: 'Yes, remove it',
+                flavor: 'danger'
+            }).then(function() {
+                ProcedureSrv.remove(procedure._id)
+                    .then(function() {
+                        self.load();
+                        NotificationSrv.success('TTP has been successfully removed');
+                    })
+                    .catch(function(err) {
+                        NotificationSrv.error('Procedure List', err.data, err.status);
+                    });
             });
         };
 
@@ -156,6 +157,12 @@
                 });
         };
 
+        this.sortBy = function(sort) {
+            self.list.sort = sort;
+            self.list.update();
+            self.filtering.setSort(sort);
+        };
+
         self.sortByField = function(field) {
             var context = this.filtering.context;
             var currentSort = Array.isArray(context.sort) ? context.sort[0] : context.sort;
@@ -171,106 +178,5 @@
             self.list.update();
             self.filtering.setSort(sort);
         };
-
-        // $scope.filterMyTasks = function() {
-        //     $scope.filtering.clearFilters()
-        //         .then(function() {
-        //             var currentUser = AuthenticationSrv.currentUser;
-        //             $scope.filtering.addFilter({
-        //                 field: 'assignee',
-        //                 type: 'user',
-        //                 value: {
-        //                     list: [{
-        //                         text: currentUser.login,
-        //                         label: currentUser.name
-        //                     }]
-        //                 }
-        //             });
-        //             $scope.search();
-        //         });
-        // };
-
-        // $scope.toggleGroupedView = function() {
-        //     $scope.state.showGrouped = !$scope.state.showGrouped;
-        //
-        //     AppLayoutSrv.groupTasks($scope.state.showGrouped);
-        // };
-
-        // $scope.buildTaskGroups = function(tasks) {
-        //     // Sort tasks by order
-        //     var orderedTasks = _.sortBy(_.map(tasks, function(t) {
-        //         return _.pick(t, 'group', 'order');
-        //     }), 'order');
-        //     var groups = [];
-        //
-        //     // Get group names by keeping the group orders
-        //     _.each(orderedTasks, function(task) {
-        //         if(groups.indexOf(task.group) === -1) {
-        //             groups.push(task.group);
-        //         }
-        //     });
-        //
-        //     var groupedTasks = [];
-        //     _.each(groups, function(group) {
-        //         groupedTasks.push({
-        //             group: group,
-        //             tasks: _.filter(tasks, function(t) {
-        //                 return t.group === group;
-        //             })
-        //         });
-        //     });
-        //
-        //     $scope.groups = groups;
-        //     $scope.groupedTasks = groupedTasks;
-        // };
-
-        // $scope.showTask = function(taskId) {
-        //     $state.go('app.case.tasks-item', {
-        //         itemId: taskId
-        //     });
-        // };
-
-        // $scope.updateField = function (fieldName, newValue, task) {
-        //     var field = {};
-        //     field[fieldName] = newValue;
-        //     return CaseTaskSrv.update({
-        //         taskId: task._id
-        //     }, field, function () {}, function (response) {
-        //         NotificationSrv.error('taskList', response.data, response.status);
-        //     });
-        // };
-
-        // $scope.addTask = function() {
-        //     CaseTaskSrv.save({
-        //         'caseId': $scope.caseId,
-        //         'flag': false
-        //     }, $scope.newTask, function() {
-        //         $scope.isNewTask = false;
-        //         $scope.newTask.title = '';
-        //         $scope.newTask.group = '';
-        //         NotificationSrv.success('Task has been successfully added');
-        //     }, function(response) {
-        //         NotificationSrv.error('taskList', response.data, response.status);
-        //     });
-        // };
-        //
-        // $scope.removeTask = function(task) {
-        //
-        //     ModalUtilsSrv.confirm('Delete task', 'Are you sure you want to delete the selected task?', {
-        //         okText: 'Yes, remove it',
-        //         flavor: 'danger'
-        //     }).then(function() {
-        //         CaseTaskSrv.update({
-        //             'taskId': task._id
-        //         }, {
-        //             status: 'Cancel'
-        //         }, function() {
-        //             $scope.$emit('tasks:task-removed', task);
-        //             NotificationSrv.success('Task has been successfully removed');
-        //         }, function(response) {
-        //             NotificationSrv.error('taskList', response.data, response.status);
-        //         });
-        //     });
-        // };
     }
 }());
