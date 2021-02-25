cadc641af94e32e127fb58daeaf9c9e7ecf4ba03
Nabil Adouani <nabil.adouani@gmail.com>
2021-02-25 08:45:10 +0100

    #1670 Review the UX of the taxonomy tags selection dialog

--- frontend/app/scripts/controllers/misc/TaxonomySelectionModalCtrl.js
+++ frontend/app/scripts/controllers/misc/TaxonomySelectionModalCtrl.js
@@ -5,26 +5,43 @@
             var self = this;
 
             this.taxonomies = angular.copy(taxonomies);
+            this.selectedTags = [];
 
             this.formData = {
                 selectedTaxonomy: null,
-                selectedTags: null
+                selectedTags: []
             };
 
-            this.addSelectedTags = function() {
-                if (!self.formData.selectedTaxonomy) {
-                    return;
-                }
+            this.selectTaxonomy = function(taxonomy) {
+                self.formData.selectedTaxonomy = taxonomy;
+            }
 
-                var selection = _.filter(self.formData.selectedTaxonomy.tags, function(tag) {
-                    return tag.selected;
+            this.selectTag = function(tag) {
+                tag.selected = !!!tag.selected;
+
+                if(tag.selected === true) {
+                    // Add to selection
+                    self.formData.selectedTags.push(tag);
+                } else {
+                    // Remove from selection
+                    self.formData.selectedTags = _.without(self.formData.selectedTags, tag);
+                }
+            }
+
+            this.clearSelection = function() {
+                _.each(self.formData.selectedTags, function(tag) {
+                    tag.selected = false;
                 });
 
-                if (selection.length === 0) {
+                self.formData.selectedTags = [];
+            }
+
+            this.addSelectedTags = function() {
+                if (!self.formData.selectedTaxonomy || self.formData.selectedTags.length === 0) {
                     return;
                 }
 
-                $uibModalInstance.close(selection);
+                $uibModalInstance.close(self.formData.selectedTags);
             };
 
             this.cancel = function() {
