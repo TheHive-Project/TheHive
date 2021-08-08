072d7f5747ca9263b00ffe4ef99f42a7562b6aa2
To-om <toom@thehive-project.org>
2021-08-08 16:52:02 +0200

    #2149 Add range in global search API

--- thehive/app/org/thp/thehive/controllers/v1/SearchCtrl.scala
+++ thehive/app/org/thp/thehive/controllers/v1/SearchCtrl.scala
@@ -2,7 +2,7 @@ package org.thp.thehive.controllers.v1
 
 import org.apache.tinkerpop.gremlin.process.traversal.P
 import org.apache.tinkerpop.gremlin.structure.Vertex
-import org.thp.scalligraph.controllers.Entrypoint
+import org.thp.scalligraph.controllers.{Entrypoint, FieldsParser}
 import org.thp.scalligraph.models._
 import org.thp.scalligraph.traversal.{IdentityConverter, Traversal}
 import org.thp.thehive.controllers.v1.Conversion._
@@ -35,57 +35,67 @@ class SearchCtrl(
       case (i, _)                                        => i
     }
 
-  def search(query: String): Action[AnyContent] =
-    entrypoint("search").auth { implicit request =>
-      val results = db.source[JsValue] { implicit graph =>
-        val filters = stdFields
-          .toSeq
-          .flatMap(f =>
-            Seq(
-              (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, TextPredicate.startsWith(query)),
-              (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, P.eq(query))
-            )
-          ) ++
-          fullTextFields
+  def search: Action[AnyContent] =
+    entrypoint("search")
+      .extract("query", FieldsParser[String].on("query"))
+      .extract("from", FieldsParser[Long].optional.on("from"))
+      .extract("to", FieldsParser[Long].optional.on("to"))
+      .auth { implicit request =>
+        val query: String = request.body("query")
+        val from: Long    = request.body("from").getOrElse(0)
+        val to: Long      = request.body("to").getOrElse(10)
+        val results = db.source[JsValue] { implicit graph =>
+          val filters = stdFields
             .toSeq
             .flatMap(f =>
               Seq(
-                (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, FullTextPredicate.contains(query)),
                 (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, TextPredicate.startsWith(query)),
                 (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, P.eq(query))
               )
             ) ++
-          fullTextOnlyFields.toSeq.map(f => (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, FullTextPredicate.contains(query)))
+            fullTextFields
+              .toSeq
+              .flatMap(f =>
+                Seq(
+                  (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, FullTextPredicate.contains(query)),
+                  (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, TextPredicate.startsWith(query)),
+                  (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, P.eq(query))
+                )
+              ) ++
+            fullTextOnlyFields
+              .toSeq
+              .map(f => (_: Traversal[Vertex, Vertex, IdentityConverter[Vertex]]).unsafeHas(f, FullTextPredicate.contains(query)))
 
-        if (filters.isEmpty) Iterator.empty
-        else
-          graph
-            .VV()
-            .or(filters: _*)
-            .chooseValue(
-              _.on(_.label)
-                .option("Alert", _.v[Alert].visible.richAlert.domainMap(_.toJson))
-                .option("Audit", _.v[Audit].visible.richAudit.domainMap(_.toJson))
-                .option("Case", _.v[Case].visible.richCase.domainMap(_.toJson))
-                .option("CaseTemplate", _.v[CaseTemplate].visible.richCaseTemplate.domainMap(_.toJson))
-                .option("CustomField", _.v[CustomField].domainMap(_.toJson))
+          if (filters.isEmpty) Iterator.empty
+          else
+            graph
+              .VV()
+              .or(filters: _*)
+              .chooseValue(
+                _.on(_.label)
+                  .option("Alert", _.v[Alert].visible.richAlert.domainMap(_.toJson))
+                  .option("Audit", _.v[Audit].visible.richAudit.domainMap(_.toJson))
+                  .option("Case", _.v[Case].visible.richCase.domainMap(_.toJson))
+                  .option("CaseTemplate", _.v[CaseTemplate].visible.richCaseTemplate.domainMap(_.toJson))
+                  .option("CustomField", _.v[CustomField].domainMap(_.toJson))
 //            .option("Dashboard", _.v[Dashboard].domainMap(_.toJson))
-                .option("Log", _.v[Log].visible.richLog.domainMap(_.toJson))
-                .option("Observable", _.v[Observable].visible.richObservable.domainMap(_.toJson))
-                .option("ObservableType", _.v[ObservableType].domainMap(_.toJson))
-                .option("Organisation", _.v[Organisation].visible.richOrganisation.domainMap(_.toJson))
+                  .option("Log", _.v[Log].visible.richLog.domainMap(_.toJson))
+                  .option("Observable", _.v[Observable].visible.richObservable.domainMap(_.toJson))
+                  .option("ObservableType", _.v[ObservableType].domainMap(_.toJson))
+                  .option("Organisation", _.v[Organisation].visible.richOrganisation.domainMap(_.toJson))
 //            //      pageCtrl,
-                .option("Pattern", _.v[Pattern].richPattern.domainMap(_.toJson))
-                .option("Procedure", _.v[Procedure].visible.richProcedure.domainMap(_.toJson))
-                .option("Profile", _.v[Profile].domainMap(_.toJson))
-                .option("Share", _.v[Share].visible.richShare.domainMap(_.toJson))
-                .option("Tag", _.v[Tag].visible.domainMap(_.toJson))
-                .option("Task", _.v[Task].visible.richTask.domainMap(_.toJson))
-                .option("Taxonomy", _.v[Taxonomy].visible.richTaxonomy.domainMap(_.toJson))
-                .option("User", _.v[User].visible.richUser.domainMap(_.toJson))
-            )
-            .toIterator
+                  .option("Pattern", _.v[Pattern].richPattern.domainMap(_.toJson))
+                  .option("Procedure", _.v[Procedure].visible.richProcedure.domainMap(_.toJson))
+                  .option("Profile", _.v[Profile].domainMap(_.toJson))
+                  .option("Share", _.v[Share].visible.richShare.domainMap(_.toJson))
+                  .option("Tag", _.v[Tag].visible.domainMap(_.toJson))
+                  .option("Task", _.v[Task].visible.richTask.domainMap(_.toJson))
+                  .option("Taxonomy", _.v[Taxonomy].visible.richTaxonomy.domainMap(_.toJson))
+                  .option("User", _.v[User].visible.richUser.domainMap(_.toJson))
+              )
+              .range(from, to)
+              .toIterator
+        }
+        Success(Results.Ok.chunked(results.map(_.toString).intersperse("[", ",", "]"), Some("application/json")))
       }
-      Success(Results.Ok.chunked(results.map(_.toString).intersperse("[", ",", "]"), Some("application/json")))
-    }
 }
